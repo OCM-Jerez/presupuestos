cd25baca471e582703fed749af87e10498dd0553
mamjerez <correomam@gmail.com>
2022-06-12 18:44:59 +0200

    Tutoria Jimy

--- src/app/tables/table-programa-details/table-programa-details.component.ts
+++ src/app/tables/table-programa-details/table-programa-details.component.ts
@@ -16,6 +16,7 @@ import { IDataTable } from '../../commons/interfaces/dataTable.interface';
 
 import { accumulate } from '../../commons/util/util';
 import { ColumnState, GridReadyEvent } from 'ag-grid-community';
+import { GridOptions, GridApi } from 'ag-grid-community/main';
 
 @Component({
   selector: 'app-table-programa-details',
@@ -36,8 +37,8 @@ export class TableProgramaDetailsComponent {
   public dataIntermedio: any;
   public dataFinal: any;
   public aplicacionesPresupuestarias: any;
-
-  private _gridApi;
+  public gridOptions: GridOptions;
+  private _gridApi: GridApi;
   private _dataTableGraph: IDataTable;
 
   constructor(
@@ -190,43 +191,62 @@ export class TableProgramaDetailsComponent {
 
     ];
 
-    this.defaultColDef = {
-      width: 110,
-      sortable: true,
-      resizable: true,
-      filter: true,
-      aggFunc: 'sum',
-      cellRenderer: CellRendererOCM,
-      headerComponentParams: {
-        template:
-          '<div class="ag-cell-label-container" role="presentation">' +
-          '  <span ref="eMenu" class="ag-header-icon ag-header-cell-menu-button" ></span>' +
-          '  <div ref="eLabel" class="ag-header-cell-label" role="presentation" >' +
-          '    <span ref="eSortOrder" class="ag-header-icon ag-sort-order"></span>' +
-          '    <span ref="eSortAsc" class="ag-header-icon ag-sort-ascending-icon"></span>' +
-          '    <span ref="eSortDesc" class="ag-header-icon ag-sort-descending-icon"></span>' +
-          '    <span ref="eSortNone" class="ag-header-icon ag-sort-none-icon"></span>' +
-          '    <span ref="eText" class="ag-header-cell-text" role="columnheader" style="white-space: normal;"></span>' +
-          '    <span ref="eFilter" class="ag-header-icon ag-filter-icon"></span>' +
-          '  </div>' +
-          '</div>',
-      },
-    };
-    this.localeText = localeTextESPes;
 
+    this.demo().then(() => {
+      this.gridOptions = {
+        defaultColDef: {
+          width: 130,
+          sortable: true,
+          resizable: true,
+          filter: true,
+          aggFunc: 'sum',
+          cellRenderer: CellRendererOCM,
+          headerComponentParams: {
+            template:
+              '<div class="ag-cell-label-container" role="presentation">' +
+              '  <span ref="eMenu" class="ag-header-icon ag-header-cell-menu-button" ></span>' +
+              '  <div ref="eLabel" class="ag-header-cell-label" role="presentation" >' +
+              '    <span ref="eSortOrder" class="ag-header-icon ag-sort-order"></span>' +
+              '    <span ref="eSortAsc" class="ag-header-icon ag-sort-ascending-icon"></span>' +
+              '    <span ref="eSortDesc" class="ag-header-icon ag-sort-descending-icon"></span>' +
+              '    <span ref="eSortNone" class="ag-header-icon ag-sort-none-icon"></span>' +
+              '    <span ref="eText" class="ag-header-cell-text" role="columnheader" style="white-space: normal;"></span>' +
+              '    <span ref="eFilter" class="ag-header-icon ag-filter-icon"></span>' +
+              '  </div>' +
+              '</div>',
+          },
+        },
 
+        // PROPERTIES - object properties, myRowData and myColDefs are created somewhere in your application
+        rowData: this.rowData,
+        columnDefs: this.columnDefs,
+        groupDisplayType: 'custom',
+        groupIncludeTotalFooter: true,
+        groupIncludeFooter: true,
+        groupHeaderHeight: 25,
+        headerHeight: 36,
+        suppressAggFuncInHeader: true,
+        rowSelection: 'single',
+        localeText: localeTextESPes,
+        pagination: false,
+
+      } as GridOptions;
+    })
   }
 
-  async onGridReady(params: GridReadyEvent) {
+  onGridReady(params: GridReadyEvent) {
     this._gridApi = params.api;
     var defaultSortModel: ColumnState[] = [
       { colId: 'DesEco', sort: 'asc', sortIndex: 0 },
     ];
     params.columnApi.applyColumnState({ state: defaultSortModel });
+  }
 
+  async demo(): Promise<void> {
     this.rowData = (await this._prepareDataProgramaDetailsService.getDataAllYear())
       .filter(x => x.CodPro == this.dataStoreService.selectedCodeRowFirstLevel.split(" ")[0]);
     // console.log(this.rowData);
+    console.log(this.rowData);
 
     // Acumular los datos por aplicación presupuestaria = orgánico + programa + económico.
     this.aplicacionesPresupuestarias = []
@@ -239,35 +259,36 @@ export class TableProgramaDetailsComponent {
     // console.log("years", years);
 
     // Creo array de aplicaciones presupuestarias existentes en programa seleccionado.
-    this.rowData.map(item => {
-      item.AplicacionPresupuestaria = item.CodOrg + '-' + item.CodPro + '-' + item.CodEco;
-      this.aplicacionesPresupuestarias.push(item.AplicacionPresupuestaria)
-      this.aplicacionesPresupuestarias = [...new Set(this.aplicacionesPresupuestarias)];
-    });
+    // this.rowData.forEach(item => {
+    //   item.AplicacionPresupuestaria = item.CodOrg + '-' + item.CodPro + '-' + item.CodEco;
+    //   this.aplicacionesPresupuestarias.push(item.AplicacionPresupuestaria)
+    //   // this.aplicacionesPresupuestarias = [...new Set(this.aplicacionesPresupuestarias)];
+    // });
+    // this.aplicacionesPresupuestarias = [...new Set(this.aplicacionesPresupuestarias)];
     // console.log("aplicacionesPresupuestarias", this.aplicacionesPresupuestarias);
 
     // Creo item para cada uno de los aplicaciones presupuestarias existentes en programa seleccionado.
-    this.aplicacionesPresupuestarias.map(item => {
-      const dataIntermedio = this.rowData.filter(x => x.AplicacionPresupuestaria === item);
-      const yearsIniciales = accumulate('Iniciales', dataIntermedio);
-      const yearsModificaciones = accumulate('Modificaciones', dataIntermedio);
-      const yearsDefinitivas = accumulate('Definitivas', dataIntermedio);
-      const yearsGastosComprometidos = accumulate('GastosComprometidos', dataIntermedio);
-      const yearsObligacionesNetas = accumulate('ObligacionesReconocidasNetas', dataIntermedio);
-      const yearsPagos = accumulate('Pagos', dataIntermedio);
-      const yearsObligacionesPendientes = accumulate('ObligacionesPendientePago', dataIntermedio);
-      const yearsRemanenteCredito = accumulate('RemanenteCredito', dataIntermedio);
+    this.rowData.forEach(item => {
+      item.AplicacionPresupuestaria = item.CodOrg + '-' + item.CodPro + '-' + item.CodEco;
+      const yearsIniciales = accumulate('Iniciales', [item]);
+      const yearsModificaciones = accumulate('Modificaciones', [item]);
+      const yearsDefinitivas = accumulate('Definitivas', [item]);
+      const yearsGastosComprometidos = accumulate('GastosComprometidos', [item]);
+      const yearsObligacionesNetas = accumulate('ObligacionesReconocidasNetas', [item]);
+      const yearsPagos = accumulate('Pagos', [item]);
+      const yearsObligacionesPendientes = accumulate('ObligacionesPendientePago', [item]);
+      const yearsRemanenteCredito = accumulate('RemanenteCredito', [item]);
 
       const value = {
         "AplicacionPresupuestaria": item,
-        "CodOrg": item.split('-')[0],
-        "CodPro": item.split('-')[1],
-        "CodEco": item.split('-')[2],
-        "CodCap": item.split('-')[2].charAt(0),
-        "DesOrg": dataIntermedio[0].DesOrg,
-        "DesPro": dataIntermedio[0].DesPro,
-        "DesCap": dataIntermedio[0].DesCap,
-        "DesEco": dataIntermedio[0].DesEco,
+        "CodOrg": item.CodOrg,
+        "CodPro": item.CodPro,
+        "CodEco": item.CodEco,
+        "CodCap": String(item.CodEco).charAt(0),
+        "DesOrg": item.DesOrg,
+        "DesPro": item.DesPro,
+        "DesCap": item.DesCap,
+        "DesEco": item.DesEco
       }
 
       const years = this.avalaibleYearsService.getYearsSelected();
@@ -283,14 +304,52 @@ export class TableProgramaDetailsComponent {
       })
       this.dataFinal.push(value)
     });
-    this.rowData = this.dataFinal;
 
+    // this.aplicacionesPresupuestarias.forEach(item => {
+    //   const dataIntermedio = this.rowData.filter(x => x.AplicacionPresupuestaria === item);
+    //   const yearsIniciales = accumulate('Iniciales', dataIntermedio);
+    //   const yearsModificaciones = accumulate('Modificaciones', dataIntermedio);
+    //   const yearsDefinitivas = accumulate('Definitivas', dataIntermedio);
+    //   const yearsGastosComprometidos = accumulate('GastosComprometidos', dataIntermedio);
+    //   const yearsObligacionesNetas = accumulate('ObligacionesReconocidasNetas', dataIntermedio);
+    //   const yearsPagos = accumulate('Pagos', dataIntermedio);
+    //   const yearsObligacionesPendientes = accumulate('ObligacionesPendientePago', dataIntermedio);
+    //   const yearsRemanenteCredito = accumulate('RemanenteCredito', dataIntermedio);
+
+    //   const value = {
+    //     "AplicacionPresupuestaria": item,
+    //     "CodOrg": item.split('-')[0],
+    //     "CodPro": item.split('-')[1],
+    //     "CodEco": item.split('-')[2],
+    //     "CodCap": item.split('-')[2].charAt(0),
+    //     "DesOrg": dataIntermedio[0].DesOrg,
+    //     "DesPro": dataIntermedio[0].DesPro,
+    //     "DesCap": dataIntermedio[0].DesCap,
+    //     "DesEco": dataIntermedio[0].DesEco,
+    //   }
+
+    //   const years = this.avalaibleYearsService.getYearsSelected();
+    //   years.forEach((year) => {
+    //     value[`Iniciales${year}`] = yearsIniciales[year];
+    //     value[`Modificaciones${year}`] = yearsModificaciones[year];
+    //     value[`Definitivas${year}`] = yearsDefinitivas[year];
+    //     value[`GastosComprometidos${year}`] = yearsGastosComprometidos[year];
+    //     value[`ObligacionesReconocidasNetas${year}`] = yearsObligacionesNetas[year];
+    //     value[`Pagos${year}`] = yearsPagos[year];
+    //     value[`ObligacionesPendientePago${year}`] = yearsObligacionesPendientes[year];
+    //     value[`RemanenteCredito${year}`] = yearsRemanenteCredito[year]
+    //   })
+    //   this.dataFinal.push(value)
+    // });
+
+    this.rowData = [...this.dataFinal];
+    // this.gridOptions.rowData = this.rowData;
+    // this._gridApi.redrawRows()
     // Necesario debido a tiempo de vida componente.
     setTimeout(() => {
       this.expandAll()
     }, 10);
   }
-
   // TODO: Las colummnas disparan su altura
   headerHeightSetter() {
     // var padding = 20;
