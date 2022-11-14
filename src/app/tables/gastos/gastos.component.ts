b5c3403597a0be3d1150dc4c81164138917c5cda
mamjerez <correomam@gmail.com>
2022-11-14 10:15:15 +0100

    Delete ngOnInit()

--- src/app/tables/gastos/gastos.component.ts
+++ src/app/tables/gastos/gastos.component.ts
@@ -1,49 +1,47 @@
-import { CLASIFICATION_TYPE } from './../../commons/util/util';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { DataStoreService } from '../../services/dataStore.service';
+import { Component, ViewChild } from '@angular/core';
+
 import { TableGastosComponent } from './components/table-gastos/table-gastos.component';
+import { TableService } from '../../services/table.service';
+import { DataStoreService } from '../../services/dataStore.service';
 import { IButtonClasification } from './model/components.interface';
 import { getClasificacion } from '../data-table';
-import { TableService } from '../../services/table.service';
+// import { CLASIFICATION_TYPE } from './../../commons/util/util';
 
 @Component({
   selector: 'app-gastos',
   templateUrl: './gastos.component.html',
   styleUrls: ['./gastos.component.scss']
 })
-export class GastosComponent implements OnInit {
+export class GastosComponent {
   @ViewChild('table') tableAg!: TableGastosComponent;
 
-  constructor(private _dataStoreService: DataStoreService, private _tableService: TableService) { }
-  buttons = getClasificacion('gastosOrganicaOrganicos').buttons;
+  constructor(
+    private _dataStoreService: DataStoreService,
+    private _tableService: TableService) { }
 
+  buttons = getClasificacion('gastosOrganicaOrganicos').buttons;
   showTable = true;
-  ngOnInit(): void {
-  }
 
   clickButton(button: IButtonClasification): void {
     const selectedCodeRowFirstLevel = this.tableAg.selectedCodeRowFirstLevel;
 
     if (selectedCodeRowFirstLevel) {
       this.showTable = false;
-
       this._dataStoreService.selectedCodeRowFirstLevel = selectedCodeRowFirstLevel;
       const dataPropertyTable = getClasificacion('gastosProgramaProgramas');
       const useStarWitch: boolean = dataPropertyTable.useStarWitch;
       const attribute: string = dataPropertyTable.attribute;
-
       this._tableService.loadDataForTypeClasification(
         false,
         'gastosProgramaProgramas',
-        { valueFilter: this._dataStoreService.selectedCodeRowFirstLevel.split(" ")[0], attribute, useStarWitch }).then(() => {
+        { valueFilter: this._dataStoreService.selectedCodeRowFirstLevel.split(" ")[0], attribute, useStarWitch })
+        .then(() => {
           setTimeout(() => {
             this.buttons = dataPropertyTable.buttons;
             this.showTable = true;
           }, 100);
 
         });
-
-
     }
   }
 }
