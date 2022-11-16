09229a91939151ea09f9b7e1ea69e5932100d97c
mamjerez <correomam@gmail.com>
2022-11-16 10:16:32 +0100

    Funcionan buttons. NO seleccion fila

--- src/app/tables/gastos/gastos.component.ts
+++ src/app/tables/gastos/gastos.component.ts
@@ -1,10 +1,14 @@
 import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
-import { TableGastosComponent } from './components/table-gastos/table-gastos.component';
+// import { TableGastosComponent } from './components/table-gastos/table-gastos.component';
 import { TableService } from '../../services/table.service';
 import { DataStoreService } from '../../services/dataStore.service';
 import { IButtonClasification } from './model/components.interface';
 import { getClasificacion } from '../data-table';
+
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColumnState, ColumnApi, GridApi, GridReadyEvent } from "ag-grid-community/main";
 // import { CLASIFICATION_TYPE } from './../../commons/util/util';
 
 @Component({
@@ -13,36 +17,72 @@ import { getClasificacion } from '../data-table';
   styleUrls: ['./gastos.component.scss']
 })
 export class GastosComponent {
-  @ViewChild('table') tableAg!: TableGastosComponent;
+  // @ViewChild('table') tableAg!: TableGastosComponent;
+  @ViewChild('agGrid', { static: false }) agGrid: AgGridAngular;
+  private _gridApi: GridApi;
+  private _columnApi: ColumnApi;
 
   constructor(
     private _dataStoreService: DataStoreService,
-    private _tableService: TableService
+    private _tableService: TableService,
+    private _router: Router,
   ) { }
 
   buttons = getClasificacion(this._dataStoreService.getDataTable.clasificationType).buttons;
   showTable = true;
 
-  clickButton(button: IButtonClasification): void {
-    const selectedCodeRowFirstLevel = this.tableAg.selectedCodeRowFirstLevel;
+  onGridReady = (params: GridReadyEvent) => {
+    this._gridApi = params.api;
+    console.log(this._gridApi);
 
-    if (selectedCodeRowFirstLevel) {
-      this.showTable = false;
-      this._dataStoreService.selectedCodeRowFirstLevel = selectedCodeRowFirstLevel;
-      const dataPropertyTable = getClasificacion('gastosProgramaProgramas');
-      const useStarWitch: boolean = dataPropertyTable.useStarWitch;
-      const attribute: string = dataPropertyTable.attribute;
-      this._tableService.loadDataForTypeClasification(
-        false,
-        'gastosProgramaProgramas',
-        { valueFilter: this._dataStoreService.selectedCodeRowFirstLevel.split(" ")[0], attribute, useStarWitch })
-        .then(() => {
-          setTimeout(() => {
-            this.buttons = dataPropertyTable.buttons;
-            this.showTable = true;
-          }, 100);
-
-        });
-    }
+    this._columnApi = params.columnApi;
+    // var defaultSortModel: ColumnState[] = [
+    //   { colId: this._dataTable.dataPropertyTable.codField, sort: 'asc', sortIndex: 0 },
+    // ];
+    // params.columnApi.applyColumnState({ state: defaultSortModel });
   }
+
+  // clickButton(button: IButtonClasification): void {
+  //   // const selectedCodeRowFirstLevel = this.tableAg.selectedCodeRowFirstLevel;
+  //   const selectedRows = this.agGrid.api.getSelectedNodes();
+
+  //   console.log(button);
+
+  //   // if (selectedCodeRowFirstLevel) {
+  //   if (selectedRows) {
+  //     this.showTable = false;
+  //     // this._dataStoreService.selectedCodeRowFirstLevel = selectedCodeRowFirstLevel;
+  //     // this._dataStoreService.selectedCodeRowFirstLevel = selectedRows;
+
+  //     const dataPropertyTable = getClasificacion('gastosProgramaProgramas');
+  //     const useStarWitch: boolean = dataPropertyTable.useStarWitch;
+  //     const attribute: string = dataPropertyTable.attribute;
+  //     this._tableService.loadDataForTypeClasification(
+  //       false,
+  //       'gastosProgramaProgramas',
+  //       { valueFilter: this._dataStoreService.selectedCodeRowFirstLevel.split(" ")[0], attribute, useStarWitch })
+  //       .then(() => {
+  //         setTimeout(() => {
+  //           this.buttons = dataPropertyTable.buttons;
+  //           this.showTable = true;
+  //         }, 100);
+
+  //       });
+  //   }
+  // }
+
+  async detalle(button: IButtonClasification) {
+    await this._tableService.loadDataForTypeClasification(
+      false,
+      button.clasificationType);
+    this.reloadCurrentRoute()
+  }
+
+  reloadCurrentRoute() {
+    let currentUrl = this._router.url;
+    this._router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this._router.navigate([currentUrl]);
+    });
+  }
+
 }
