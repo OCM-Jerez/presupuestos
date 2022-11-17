10ee663c6088e4197af1882fad03c5bb3ecaec47
mamjerez <correomam@gmail.com>
2022-11-17 17:00:09 +0100

    Funciona filtrado

--- src/app/tables/gastos/gastos.component.ts
+++ src/app/tables/gastos/gastos.component.ts
@@ -42,39 +42,23 @@ export class GastosComponent {
     // params.columnApi.applyColumnState({ state: defaultSortModel });
   }
 
-  // clickButton(button: IButtonClasification): void {
-  //   // const selectedCodeRowFirstLevel = this.tableAg.selectedCodeRowFirstLevel;
-  //   const selectedRows = this.agGrid.api.getSelectedNodes();
-
-  //   console.log(button);
-
-  //   // if (selectedCodeRowFirstLevel) {
-  //   if (selectedRows) {
-  //     this.showTable = false;
-  //     // this._dataStoreService.selectedCodeRowFirstLevel = selectedCodeRowFirstLevel;
-  //     // this._dataStoreService.selectedCodeRowFirstLevel = selectedRows;
-
-  //     const dataPropertyTable = getClasificacion('gastosProgramaProgramas');
-  //     const useStarWitch: boolean = dataPropertyTable.useStarWitch;
-  //     const attribute: string = dataPropertyTable.attribute;
-  //     this._tableService.loadDataForTypeClasification(
-  //       false,
-  //       'gastosProgramaProgramas',
-  //       { valueFilter: this._dataStoreService.selectedCodeRowFirstLevel.split(" ")[0], attribute, useStarWitch })
-  //       .then(() => {
-  //         setTimeout(() => {
-  //           this.buttons = dataPropertyTable.buttons;
-  //           this.showTable = true;
-  //         }, 100);
-
-  //       });
-  //   }
-  // }
-
   async detalle(button: IButtonClasification) {
-    await this._tableService.loadDataForTypeClasification(
-      false,
-      button.clasificationType);
+    const dataPropertyTable = getClasificacion(button.clasificationType);
+
+    if (this._dataStoreService.selectedCodeRowFirstLevel) {
+      const useStarWitch: boolean = dataPropertyTable.useStarWitch;
+      const attribute: string = dataPropertyTable.attribute;
+      await this._tableService.loadDataForTypeClasification(
+        false,
+        button.clasificationType,
+        { valueFilter: this._dataStoreService.selectedCodeRowFirstLevel.split(" ")[0], attribute, useStarWitch });
+    } else {
+      await this._tableService.loadDataForTypeClasification(
+        false,
+        button.clasificationType);
+    }
+
+    this._dataStoreService.selectedCodeRowFirstLevel = '';
     this.reloadCurrentRoute()
   }
 
