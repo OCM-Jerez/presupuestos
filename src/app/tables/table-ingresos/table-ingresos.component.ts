3cde23b17ba61b1a46c75ca346cc74b41ba6cb34
mamjerez <correomam@gmail.com>
2022-11-18 04:04:10 +0100

    Clean code opciones porConcepto() porArticulo()

--- src/app/tables/table-ingresos/table-ingresos.component.ts
+++ src/app/tables/table-ingresos/table-ingresos.component.ts
@@ -240,7 +240,6 @@ export class TableIngresosComponent {
     const selectedRows = this.agGrid.api.getSelectedNodes();
     const dataPropertyTable = getClasificacion(typeClasification);
 
-
     if (selectedRows.length > 0) {
       this._dataStoreService.selectedCodeRowFirstLevel = selectedRows[0].key;
       const useStarWitch: boolean = dataPropertyTable.useStarWitch;
@@ -249,80 +248,15 @@ export class TableIngresosComponent {
         true,
         typeClasification,
         { valueFilter: this._dataStoreService.selectedCodeRowFirstLevel.split(" ")[0], attribute, useStarWitch });
-      // this.reloadCurrentRoute()
     } else {
       await this._tableService.loadDataForTypeClasification(
         true,
         typeClasification);
       // this._alertService.showAlert(`Selecciona artículo`);
     }
+
     this.reloadCurrentRoute()
-
   }
-
-  // async porArticulo() {
-  //   this._dataStoreService.IsDetails = true;
-  //   const selectedRows = this.agGrid.api.getSelectedNodes();
-  //   const typeClasification: CLASIFICATION_TYPE = 'ingresosEconomicaArticulos';
-  //   const dataPropertyTable = getClasificacion(typeClasification);
-
-  //   if (selectedRows.length > 0) {
-  //     this._dataStoreService.selectedCodeRowFirstLevel = selectedRows[0].key;
-  //     const useStarWitch: boolean = dataPropertyTable.useStarWitch;
-  //     const attribute: string = dataPropertyTable.attribute;
-  //     await this._tableService.loadDataForTypeClasification(
-  //       true,
-  //       typeClasification,
-  //       { valueFilter: this._dataStoreService.selectedCodeRowFirstLevel.split(" ")[0], attribute, useStarWitch });
-  //     this.reloadCurrentRoute()
-  //   } else {
-  //     await this._tableService.loadDataForTypeClasification(
-  //       true,
-  //       typeClasification);
-  //     this.reloadCurrentRoute()
-  //     // this._alertService.showAlert(`Selecciona artículo`);
-  //   }
-  // }
-
-  // async porConcepto() {
-  //   this._dataStoreService.IsDetails = true;
-  //   const selectedRows = this.agGrid.api.getSelectedNodes();
-
-  //   if (selectedRows.length > 0) {
-  //     this._dataStoreService.selectedCodeRowFirstLevel = selectedRows[0].key;
-  //     const typeClasification: CLASIFICATION_TYPE = 'ingresosEconomicaConceptos';
-  //     const dataPropertyTable = getClasificacion(typeClasification);
-  //     const useStarWitch: boolean = dataPropertyTable.useStarWitch;
-  //     const attribute: string = dataPropertyTable.attribute;
-
-  //     await this._tableService.loadDataForTypeClasification(
-  //       true,
-  //       typeClasification,
-  //       { valueFilter: this._dataStoreService.selectedCodeRowFirstLevel.split(" ")[0], attribute, useStarWitch });
-  //     this.reloadCurrentRoute()
-  //   } else {
-  //     this._alertService.showAlert(`Selecciona artículo`);
-  //   }
-  // }
-
-  // async porEconomico() {
-  //   this._dataStoreService.IsDetails = true;
-  //   const selectedRows = this.agGrid.api.getSelectedNodes();
-  //   if (selectedRows.length > 0) {
-  //     this._dataStoreService.selectedCodeRowFirstLevel = selectedRows[0].key;
-  //     let typeClasification: CLASIFICATION_TYPE = 'ingresosEconomicaEconomicos';
-  //     let useStarWitch: boolean = this._dataTable.dataPropertyTable.useStarWitch;
-  //     let attribute: string = this._dataTable.dataPropertyTable.attribute;
-
-  //     await this._tableService.loadDataForTypeClasification(
-  //       true,
-  //       typeClasification,
-  //       { valueFilter: this._dataStoreService.selectedCodeRowFirstLevel.split(" ")[0], attribute, useStarWitch });
-  //     this.reloadCurrentRoute()
-  //   } else {
-  //     this._alertService.showAlert(`Selecciona económico`);
-  //   }
-  // }
 
   reloadCurrentRoute() {
     let currentUrl = this._router.url;
