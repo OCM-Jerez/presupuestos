92ea84a6c9259510b081f3266c1825da9b673709
mamjerez <correomam@gmail.com>
2022-11-22 20:18:15 +0100

    Add   @Output() clickDetail = new EventEmitter();   @Input() fieldsHide: string[] = [];

--- src/app/tables/table-ingresos/table-ingresos.component.ts
+++ src/app/tables/table-ingresos/table-ingresos.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AgGridAngular } from 'ag-grid-angular';
@@ -24,8 +24,11 @@ import { getClasificacion } from '../data-table';
   selector: 'app-compara-ing',
   templateUrl: './table-ingresos.component.html',
 })
-export class TableIngresosComponent {
+export class TableIngresosComponent implements OnInit {
   @ViewChild('agGrid', { static: false }) agGrid: AgGridAngular;
+  @Output() clickDetail = new EventEmitter();
+  @Input() fieldsHide: string[] = [];
+
   public gridOptions: GridOptions;
   public hasGraphTree = true;
   public textButton: string;
@@ -43,6 +46,11 @@ export class TableIngresosComponent {
     private _tableService: TableService
   ) {
     this._loadPropertyTable();
+  }
+  ngOnInit(): void {
+
+
+
   }
   messageYears = this.avalaibleYearsService.message;
 
@@ -131,14 +139,29 @@ export class TableIngresosComponent {
   // No lo uso en este componente pero si quisiese hacer alguna llamada a las API
   // este codigo es necesario.
   onGridReady = (params: GridReadyEvent) => {
+
     this._gridApi = params.api;
     this._columnApi = params.columnApi;
+
     var defaultSortModel: ColumnState[] = [
       { colId: this._dataTable.dataPropertyTable.codField, sort: 'asc', sortIndex: 0 },
     ];
     params.columnApi.applyColumnState({ state: defaultSortModel });
+    this._hideColumns();
   }
 
+  private _hideColumns() {
+    if (this.fieldsHide.length > 0) {
+      const year = this.avalaibleYearsService.yearsSelected[0];
+
+      const columnsHide = this.fieldsHide.map((item) => { return { colId: `${item}${year}`, hide: true } });
+      console.log(columnsHide);
+
+      this._columnApi!.applyColumnState({
+        state: columnsHide
+      });
+    }
+  }
   // TODO: Las colummnas disparan su altura
   // headerHeightSetter() {
   // var padding = 20;
@@ -148,6 +171,7 @@ export class TableIngresosComponent {
   // }
 
   createColumnsChildren(year: number) {
+
     return [
       {
         headerName: 'Créditos',
@@ -155,7 +179,7 @@ export class TableIngresosComponent {
           {
             headerName: 'Previsiones iniciales',
             field: `Iniciales${year}`,
-            columnGroupShow: 'open'
+            columnGroupShow: 'open',
           },
           {
             headerName: 'Total modificaciones',
@@ -255,7 +279,9 @@ export class TableIngresosComponent {
       // this._alertService.showAlert(`Selecciona artículo`);
     }
 
-    this.reloadCurrentRoute()
+    this.clickDetail.emit();
+
+    //this.reloadCurrentRoute()
   }
 
   reloadCurrentRoute() {
