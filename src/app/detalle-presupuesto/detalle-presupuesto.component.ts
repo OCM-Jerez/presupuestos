27652a5a1ba41d548412d30ffbb450db47a68f0a
mamjerez <correomam@gmail.com>
2022-11-25 22:56:26 +0100

    FUNCIONA treemap con datos manuales

--- src/app/detalle-presupuesto/detalle-presupuesto.component.ts
+++ src/app/detalle-presupuesto/detalle-presupuesto.component.ts
@@ -1,3 +1,4 @@
+import { HighchartsChartModule } from 'highcharts-angular';
 import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -11,8 +12,13 @@ import { CLASIFICATION_TYPE } from '../commons/util/util';
 import { getClasificacion } from '../tables/data-table';
 
 import * as Highcharts from 'highcharts';
-// import HC_exporting from 'highcharts/highcharts-more';
-// HC_exporting(Highcharts);
+import HighchartsMore from 'highcharts/highcharts-more';
+import HighchartsSolidGauge from 'highcharts/modules/solid-gauge';
+import HighchartsTreemap from 'highcharts/modules/treemap';
+
+HighchartsMore(Highcharts);
+HighchartsSolidGauge(Highcharts);
+HighchartsTreemap(Highcharts);
 
 
 @Component({
@@ -26,6 +32,10 @@ export class DetallePresupuestoComponent {
   showComponentIngresos = false;
   private typeClasification: CLASIFICATION_TYPE = 'ingresosEconomicaArticulos'
 
+  public ngAfterViewInit(): void {
+    this.createChartPie();
+  }
+
   constructor(
     public avalaibleYearsService: AvalaibleYearsService,
     private _router: Router,
@@ -35,21 +45,81 @@ export class DetallePresupuestoComponent {
     this._loadData();
   }
 
-  Highcharts = Highcharts;
-  chart: any = {
-    series: [
-      {
-        data: [1, 2, 3],
-      },
-    ],
-    chart: {
-      type: 'line',
-    },
-    title: {
-      text: 'Gráfico de prueba',
-    },
-  };
+  // https://stackblitz.com/edit/angular14-standaone-components-highcharts?file=src%2Fapp%2Fapp.component.ts
+  // https://stackblitz.com/edit/angular-9nkrgd?file=src%2Fapp%2Fapp.component.ts
+  // https://stackblitz.com/edit/angular13-highcharts-tjumvu?file=src%2Fapp%2Fapp.component.ts
+  // Highcharts = Highcharts;
+  // chart: any = {
+  //   series: [
+  //     {
+  //       data: [1, 2, 3],
+  //     },
+  //   ],
+  //   chart: {
+  //     type: 'line',
+  //   },
+  //   title: {
+  //     text: 'Gráfico de prueba',
+  //   },
+  // };
 
+
+  private createChartPie(): void {
+    const data: any[] = [{
+      name: 'A',
+      value: 6,
+      colorValue: 1
+    }, {
+      name: 'B',
+      value: 6,
+      colorValue: 2
+    }, {
+      name: 'C',
+      value: 4,
+      colorValue: 3
+    }, {
+      name: 'D',
+      value: 3,
+      colorValue: 4
+    }, {
+      name: 'E',
+      value: 2,
+      colorValue: 5
+    }, {
+      name: 'F',
+      value: 2,
+      colorValue: 6
+    }, {
+      name: 'policia',
+      value: 16,
+      colorValue: 7
+    }]
+
+    const chart = Highcharts.chart('treemap', {
+      chart: {
+        type: 'treemap',
+      },
+      title: {
+        text: '',
+      },
+      credits: {
+        enabled: false,
+      },
+      tooltip: {
+        headerFormat: `<span class="mb-2">Programa: {point.key}</span><br>`,
+        pointFormat: '<span>Euros: {point.value}</span>',
+        useHTML: true,
+      },
+      series: [
+        {
+          name: null,
+          innerSize: '50%',
+          data,
+        },
+      ],
+    } as any);
+
+  }
 
   private async _loadData(): Promise<void> {
     await this._tableService.loadDataForTypeClasification(true, this.typeClasification);
