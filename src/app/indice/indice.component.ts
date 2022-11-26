4af882569d7a5d2c8a6735e55c317664df9b5752
mamjerez <correomam@gmail.com>
2022-11-26 22:01:32 +0100

    Add menu navegacion

--- src/app/indice/indice.component.ts
+++ src/app/indice/indice.component.ts
@@ -40,4 +40,25 @@ export class IndiceComponent {
     this._router.navigateByUrl('/homeNew')
   }
 
+
+  explicamos() {
+    this._router.navigateByUrl('/explicamos')
+  }
+
+  glosario() {
+    this._router.navigateByUrl('/glosario')
+  }
+
+  visionGlobal() {
+    this._router.navigateByUrl('/home')
+  }
+
+  detallePresupuesto() {
+    this._router.navigateByUrl('/detallePresupuesto')
+  }
+
+  licitaciones() {
+    window.open('https://con.ocmjerez.org/', '_blank');
+  }
+
 }
