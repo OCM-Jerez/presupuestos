22780ab1466ed6bcfdf0bab59b90bf4bea21a435
mamjerez <correomam@gmail.com>
2022-11-27 19:32:58 +0100

    Clean code

--- src/app/commons/components/checkbox/checkbox.module.ts
+++ src/app/commons/components/checkbox/checkbox.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { CheckboxComponent } from './checkbox.component';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgSelectModule } from '@ng-select/ng-select';
-import { AppRoutingModule } from '../../../app-routing.module';
-import { PruebaModule } from '../../../hightcharts/prueba/prueba.module';
-
-
 
 @NgModule({
   declarations: [CheckboxComponent],
@@ -17,14 +11,7 @@ import { PruebaModule } from '../../../hightcharts/prueba/prueba.module';
   ],
   imports: [
     CommonModule,
-    // BrowserModule,
-    // HttpClientModule,
-    // AppRoutingModule,
     FormsModule,
-    // NgSelectModule,
-    // ReactiveFormsModule,
-    //DetallePresupuestoModule,
-    // PruebaModule
   ]
 })
 export class CheckboxModule { }
