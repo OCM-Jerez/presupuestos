68de84f06e4d55b90d001bb602e24c62c6b541a5
mamjerez <correomam@gmail.com>
2022-11-27 19:33:12 +0100

    Clean code

--- src/app/detalle-presupuesto/detalle-presupuesto.module.ts
+++ src/app/detalle-presupuesto/detalle-presupuesto.module.ts
@@ -1,21 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
 import { DetallePresupuestoComponent } from './detalle-presupuesto.component';
 import { AgGridModule } from 'ag-grid-angular';
 import { RouterModule, Routes } from '@angular/router';
 import { TableIngresosModule } from '../tables/table-ingresos/table-ingresos.module';
-import { PruebaModule } from '../hightcharts/prueba/prueba.module';
 import { HighchartsChartModule } from 'highcharts-angular';
-import { CheckboxComponent } from '../commons/components/checkbox/checkbox.component';
 import { CheckboxModule } from '../commons/components/checkbox/checkbox.module';
 
 export const routes: Routes = [{ path: '', component: DetallePresupuestoComponent }]
-
 @NgModule({
   declarations: [
     DetallePresupuestoComponent
   ],
-  imports: [RouterModule.forChild(routes),
+  imports: [
+    RouterModule.forChild(routes),
     CommonModule,
     AgGridModule,
     TableIngresosModule,
