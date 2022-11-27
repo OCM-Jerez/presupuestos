494252880047aade09e82397333d021ab8596df4
mamjerez <correomam@gmail.com>
2022-11-27 12:53:54 +0100

    Clean code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,29 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
-
-
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
   constructor(
     private _router: Router,
   ) { }
 
   ngOnInit(): void {
-  }
-
-  explicamos() {
-    this._router.navigateByUrl('/explicamos')
-  }
-
-  glosario() {
-    this._router.navigateByUrl('/glosario')
   }
 
   visionGlobal() {
