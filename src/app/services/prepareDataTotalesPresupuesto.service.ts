aa118ed115297e51a412087dc0ed6ed75c688563
mamjerez@gmail.com <mamjerez@gmail.com>
2022-12-11 00:17:41 +0100

    Clean code

--- src/app/services/prepareDataTotalesPresupuesto.service.ts
+++ src/app/services/prepareDataTotalesPresupuesto.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { IDataTable } from '../commons/interfaces/dataTable.interface';
 import { IDataTotalesPresupuesto } from '../commons/interfaces/dataTotalesPresupuesto. interface';
 import { DataStoreService } from './dataStore.service';
 import { TableService } from './table.service';
@@ -8,9 +7,6 @@ import { TableService } from './table.service';
   providedIn: 'root'
 })
 export class PrepareDataTotalesPresupuestoService {
-  private _dataTableIngresos: IDataTable;
-  private _dataTableGastos: IDataTable;
-
   private _totalPresupuestoIngresos: any;
   private _totalPresupuestoGastos: any;
 
@@ -20,7 +16,7 @@ export class PrepareDataTotalesPresupuestoService {
   ) { }
 
   async calcTotales() {
-    // Si no se hace el setTimeout no funciona
+    // Si no se hacen los dos setTimeout no funciona
     setTimeout(() => {
       this.calcPresupuestoIngresos();
     }, 0);
@@ -34,8 +30,6 @@ export class PrepareDataTotalesPresupuestoService {
 
   async calcPresupuestoIngresos() {
     await this._tableService.loadDataForTypeClasification('ingresosEconomicaArticulos');
-    // this._dataTableIngresos = this._dataStoreService.getDataTable;
-    // const dataIngresos = this._dataTableIngresos.rowData;
     this._totalPresupuestoIngresos = this._dataStoreService.getDataTable.rowData.reduce((acc, curr) => {
       Object.keys(curr).forEach((key, index) => {
         if (!acc[key]) {
@@ -45,14 +39,10 @@ export class PrepareDataTotalesPresupuestoService {
       })
       return acc
     }, {})
-    // console.clear();
-    // console.log(this._totalPresupuestoIngresos);
   }
 
   async calcPresupuestoGastos() {
     await this._tableService.loadDataForTypeClasification('gastosOrganicaOrganicos');
-    // this._dataTableGastos = this._dataStoreService.getDataTable
-    // const dataGastos = this._dataTableGastos.rowData;
     this._totalPresupuestoGastos = this._dataStoreService.getDataTable.rowData.reduce((acc, curr) => {
       Object.keys(curr).forEach((key, index) => {
         if (!acc[key]) {
@@ -62,7 +52,6 @@ export class PrepareDataTotalesPresupuestoService {
       })
       return acc
     }, {})
-    // console.log(this._totalPresupuestoGastos);
   }
 
   async setTotalesPresupuesto() {
@@ -78,7 +67,6 @@ export class PrepareDataTotalesPresupuestoService {
         totalEjecutadoGastos: this._totalPresupuestoGastos.Pagos2022.toString()
           .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.'),
       }
-      // console.log(DataTotalesPresupuesto);
       this._dataStoreService.setDataTotalesPresupuesto = DataTotalesPresupuesto;
     } catch (error) {
       // console.clear();
