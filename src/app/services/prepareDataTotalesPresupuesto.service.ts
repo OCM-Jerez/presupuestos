d613aad74e5c0a7930536495dadd50670cf6dc00
mamjerez@gmail.com <mamjerez@gmail.com>
2022-12-11 00:12:55 +0100

    Si no se hace el setTimeout, no funciona

--- src/app/services/prepareDataTotalesPresupuesto.service.ts
+++ src/app/services/prepareDataTotalesPresupuesto.service.ts
@@ -11,17 +11,32 @@ export class PrepareDataTotalesPresupuestoService {
   private _dataTableIngresos: IDataTable;
   private _dataTableGastos: IDataTable;
 
+  private _totalPresupuestoIngresos: any;
+  private _totalPresupuestoGastos: any;
+
   constructor(
     private _tableService: TableService,
     private _dataStoreService: DataStoreService,
   ) { }
 
   async calcTotales() {
+    // Si no se hace el setTimeout no funciona
+    setTimeout(() => {
+      this.calcPresupuestoIngresos();
+    }, 0);
+
+    setTimeout(() => {
+      this.calcPresupuestoGastos();
+    }, 0);
+
+    this.setTotalesPresupuesto();
+  }
+
+  async calcPresupuestoIngresos() {
     await this._tableService.loadDataForTypeClasification('ingresosEconomicaArticulos');
-    this._dataTableIngresos = this._dataStoreService.getDataTable
-    const dataIngresos = this._dataTableIngresos.rowData;
-
-    const totalPresupuestoIngresos = dataIngresos.reduce((acc, curr) => {
+    // this._dataTableIngresos = this._dataStoreService.getDataTable;
+    // const dataIngresos = this._dataTableIngresos.rowData;
+    this._totalPresupuestoIngresos = this._dataStoreService.getDataTable.rowData.reduce((acc, curr) => {
       Object.keys(curr).forEach((key, index) => {
         if (!acc[key]) {
           acc[key] = 0
@@ -30,12 +45,15 @@ export class PrepareDataTotalesPresupuestoService {
       })
       return acc
     }, {})
-    // console.log(totalPresupuestoIngresos);
+    // console.clear();
+    // console.log(this._totalPresupuestoIngresos);
+  }
 
+  async calcPresupuestoGastos() {
     await this._tableService.loadDataForTypeClasification('gastosOrganicaOrganicos');
-    this._dataTableGastos = this._dataStoreService.getDataTable
-    const dataGastos = this._dataTableGastos.rowData;
-    const totalPresupuestoGastos = dataGastos.reduce((acc, curr) => {
+    // this._dataTableGastos = this._dataStoreService.getDataTable
+    // const dataGastos = this._dataTableGastos.rowData;
+    this._totalPresupuestoGastos = this._dataStoreService.getDataTable.rowData.reduce((acc, curr) => {
       Object.keys(curr).forEach((key, index) => {
         if (!acc[key]) {
           acc[key] = 0
@@ -44,25 +62,29 @@ export class PrepareDataTotalesPresupuestoService {
       })
       return acc
     }, {})
-    // console.log(totalPresupuestoGastos);
-    // console.log(totalPresupuestoIngresos);
+    // console.log(this._totalPresupuestoGastos);
+  }
 
+  async setTotalesPresupuesto() {
     try {
       const DataTotalesPresupuesto: IDataTotalesPresupuesto = {
         year: 2022,
-        totalPresupuestoIngresos: totalPresupuestoIngresos.Definitivas2022.toString()
+        totalPresupuestoIngresos: this._totalPresupuestoIngresos.Definitivas2022.toString()
           .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.'),
-        totalEjecutadoIngresos: totalPresupuestoIngresos.DerechosReconocidosNetos2022.toString()
+        totalEjecutadoIngresos: this._totalPresupuestoIngresos.DerechosReconocidosNetos2022.toString()
           .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.'),
-        totalPresupuestoGastos: totalPresupuestoGastos.Definitivas2022.toString()
+        totalPresupuestoGastos: this._totalPresupuestoGastos.Definitivas2022.toString()
           .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.'),
-        totalEjecutadoGastos: totalPresupuestoGastos.Pagos2022.toString()
+        totalEjecutadoGastos: this._totalPresupuestoGastos.Pagos2022.toString()
           .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.'),
       }
       // console.log(DataTotalesPresupuesto);
       this._dataStoreService.setDataTotalesPresupuesto = DataTotalesPresupuesto;
     } catch (error) {
+      // console.clear();
       console.error('error------------------- ', error);
+      console.log(this._totalPresupuestoIngresos);
+      console.log(this._totalPresupuestoGastos);
     }
   }
 }
