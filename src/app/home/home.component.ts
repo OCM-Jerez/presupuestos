b2404b1924038aa32cd54e66b7fe3ecad7fc7c9d
mamjerez@gmail.com <mamjerez@gmail.com>
2022-12-26 07:32:46 +0100

    Optimizo codigo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { DataStoreService } from '../services/dataStore.service';
 import { TableService } from '../services/table.service';
 
 import { IDataTable } from '../commons/interfaces/dataTable.interface';
+import { CLASIFICATION_TYPE } from '../commons/util/util';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,117 +12,67 @@ import { IDataTable } from '../commons/interfaces/dataTable.interface';
 })
 export class HomeComponent implements OnInit {
   private _dataTable: IDataTable;
-  private _arrRandom: number[] = [];
+  private _array = [];
+  // private _arrRandom: number[] = [];
+  textoTabla: string;
   textEjemplo1: string;
   textEjemplo2: string;
   textEjemplo3: string;
   valueEjemplo1: number;
   valueEjemplo2: number;
   valueEjemplo3: number;
-  textoTabla: string;
 
   constructor(
-    private _router: Router,
-    private _dataStoreService: DataStoreService,
-    private _tableService: TableService
+    private _tableService: TableService,
+    private _router: Router
   ) { }
 
   ngOnInit(): void {
-    this._loadData()
+    this._randomData()
   }
 
-  private async _loadData(): Promise<void> {
-    //  Calculo totales de ingresos y gastos y los guardo en _dataStoreService  */
-    /* #region datos aleatorios para la tabla de ejemplos  */
+  private async _randomData(): Promise<void> {
     const ingresoGasto = (Math.random() >= 0.5) ? true : false;
+
     if (ingresoGasto) {
-      await this._tableService.loadDataForTypeClasification('ingresosEconomicaArticulos');
-      this._dataTable = this._dataStoreService.getDataTable
-      let dataIngresos = this._dataTable.rowData;
-
-      let array = [];
-      array = dataIngresos.reduce((acc, curr) => {
-        const item =
-        {
-          "name": curr['DesArt'],
-          "value": curr['DerechosReconocidosNetos2022'],
-        }
-        acc.push(item);
-        return acc;
-      }, []);
-
-      // Totalizo
-      const dataTablaAleatoria = array.reduce((acc, { name, value }) => {
-        const item = acc.find(item => item.name === name)
-        item ? item.value += value : acc.push({
-          name,
-          value,
-        });
-        return acc;
-      }, []);
-
-      this.random(1, dataTablaAleatoria.length - 1);
-      const index1 = this._arrRandom[0];
-      const index2 = this._arrRandom[1];
-      const index3 = this._arrRandom[2];
-
-      this.textoTabla = '¿Cuanto recauda el Ayuntamiento por...?'
-      try {
-        this.textEjemplo1 = dataTablaAleatoria[index1].name
-        this.valueEjemplo1 = dataTablaAleatoria[index1].value.toLocaleString();
-        this.textEjemplo2 = dataTablaAleatoria[index2].name
-        this.valueEjemplo2 = dataTablaAleatoria[index2].value.toLocaleString();
-        this.textEjemplo3 = dataTablaAleatoria[index3].name
-        this.valueEjemplo3 = dataTablaAleatoria[index3].value.toLocaleString();
-      } catch (error) {
-        console.log('error', index1, index2, index3);
-      }
+      await this.getData('ingresosEconomicaArticulos', 'DesArt', 'DerechosReconocidosNetos2022');
     } else {
-      await this._tableService.loadDataForTypeClasification('gastosOrganicaOrganicos');
-      this._dataTable = this._dataStoreService.getDataTable
-      let dataGastos = this._dataTable.rowData;
-
-      let array = [];
-      array = dataGastos.reduce((acc, curr) => {
-        const item =
-        {
-          "name": curr['DesOrg'],
-          "value": curr['Pagos2022'],
-        }
-        acc.push(item);
-        return acc;
-      }, []);
-
-      //  Totalizo
-      const dataTablaAleatoria = array.reduce((acc, { name, value }) => {
-        const item = acc.find(item => item.name === name)
-        item ? item.value += value : acc.push({
-          name,
-          value,
-        });
-        return acc;
-      }, []);
-
-      this.random(1, dataTablaAleatoria.length - 1);
-      const index1 = this._arrRandom[0];
-      const index2 = this._arrRandom[1];
-      const index3 = this._arrRandom[2];
-
-      this.textoTabla = '¿Cuanto ha gastado la delegación de...?'
-      try {
-        this.textEjemplo1 = dataTablaAleatoria[index1].name;
-        this.valueEjemplo1 = dataTablaAleatoria[index1].value.toLocaleString();
-        this.textEjemplo2 = dataTablaAleatoria[index2].name;
-        this.valueEjemplo2 = dataTablaAleatoria[index2].value.toLocaleString();
-        this.textEjemplo3 = dataTablaAleatoria[index3].name;
-        this.valueEjemplo3 = dataTablaAleatoria[index3].value.toLocaleString();
-      } catch (error) {
-        console.log('error', index1, index2, index3);
-
-      }
-
+      await this.getData('gastosOrganicaOrganicos', 'DesOrg', 'Pagos2022');
     }
-    /* #endregion */
+
+    this.textoTabla = ingresoGasto ? '¿Cuanto recauda el Ayuntamiento por...?' : '¿Cuanto ha gastado la delegación de...?';
+
+    // Totalizo
+    const dataTablaAleatoria = this._array.reduce((acc, { name, value }) => {
+      const item = acc.find(item => item.name === name)
+      item ? item.value += value : acc.push({
+        name,
+        value,
+      });
+      return acc;
+    }, []);
+
+    dataTablaAleatoria.sort(() => Math.random() - 0.5);
+    this.textEjemplo1 = dataTablaAleatoria[0].name;
+    this.valueEjemplo1 = dataTablaAleatoria[0].value.toLocaleString();
+    this.textEjemplo2 = dataTablaAleatoria[1].name;
+    this.valueEjemplo2 = dataTablaAleatoria[1].value.toLocaleString();
+    this.textEjemplo3 = dataTablaAleatoria[2].name;
+    this.valueEjemplo3 = dataTablaAleatoria[2].value.toLocaleString();
+  }
+
+  async getData(typeClasification: CLASIFICATION_TYPE, name: string, value: string) {
+    this._dataTable = await this._tableService.loadDataForTypeClasification(typeClasification);
+    let data = this._dataTable.rowData;
+    this._array = data.reduce((acc, curr) => {
+      const item =
+      {
+        "name": curr[`${name}`],
+        "value": curr[value],
+      }
+      acc.push(item);
+      return acc;
+    }, []);
   }
 
   visionGlobal() {
@@ -141,12 +91,29 @@ export class HomeComponent implements OnInit {
     this._router.navigateByUrl('/empleados')
   }
 
-  random(min: number, max: number) {
-    while (this._arrRandom.length < 3) {
-      const r = Math.floor((Math.random() * (max - min + 1)) + min);
-      if (this._arrRandom.indexOf(r) === -1) this._arrRandom.push(r);
-    }
-  }
+
+  // this.random(1, dataTablaAleatoria.length - 1);
+  // const index1 = this._arrRandom[0];
+  // const index2 = this._arrRandom[1];
+  // const index3 = this._arrRandom[2];
+
+  // try {
+  //   this.textEjemplo1 = dataTablaAleatoria[index1].name
+  //   this.valueEjemplo1 = dataTablaAleatoria[index1].value.toLocaleString();
+  //   this.textEjemplo2 = dataTablaAleatoria[index2].name
+  //   this.valueEjemplo2 = dataTablaAleatoria[index2].value.toLocaleString();
+  //   this.textEjemplo3 = dataTablaAleatoria[index3].name
+  //   this.valueEjemplo3 = dataTablaAleatoria[index3].value.toLocaleString();
+  // } catch (error) {
+  //   console.log('error', index1, index2, index3);
+  // }
+
+  // random(min: number, max: number) {
+  //   while (this._arrRandom.length < 3) {
+  //     const r = Math.floor((Math.random() * (max - min + 1)) + min);
+  //     if (this._arrRandom.indexOf(r) === -1) this._arrRandom.push(r);
+  //   }
+  // }
 
 }
 
