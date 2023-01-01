9c90b32a1c88aad1e27bc181f7d80512cd1f36ff
mamjerez@gmail.com <mamjerez@gmail.com>
2023-01-01 20:49:43 +0100

    Estructuro calculos en funciones

--- src/app/indice/indice.component.ts
+++ src/app/indice/indice.component.ts
@@ -43,6 +43,8 @@ export class IndiceComponent implements OnInit {
   private _dataTable: IDataTable;
   private _dataIngreso: any;
   private _dataGasto: any;
+  private _capitulosGastos = [];
+  private _politicasGastos = [];
   // private _typeClasification: CLASIFICATION_TYPE = 'ingresosEconomicaCapitulos'
   // https://stackoverflow.com/questions/69549927/how-to-pass-enum-value-in-angular-template-as-an-input
 
@@ -74,25 +76,20 @@ export class IndiceComponent implements OnInit {
   /* #endregion */
 
   private async _loadData(): Promise<void> {
-    /* #region  Capitulos de ingresos */
-    // this._dataTable = await this._tableService.loadData('ingresosEconomicaCapitulos');
-    // this._dataIngreso = this._dataTable.rowDataIngresos;
+    await this.calcSumIngresos();
+    await this.calcTotalesPresupuestoIngresos();
+    await this.calcSumPoliticasGastos();
+    await this.calcSumGastos();
+    await this.calcTotalesPresupuestoGastos();
+    await this.calcIndicadores();
+
+    this.showGraph()
+  }
+
+  async calcSumIngresos() {
     this._dataIngreso = this._dataStoreService.dataTable.rowDataIngresos;
 
     // Creo array de Capitulos de ingresos.
-    // let capitulos = []
-    // data.map(item => {
-    //   const value = {
-    //     "name": item.CodCap + '-' + item.DesCap,
-    //     "value": item.Definitivas2022,
-    //     "recaudado": item.DerechosReconocidosNetos2022,
-    //     "colorValue": (item.Definitivas2022 / 100)
-    //   }
-    //   capitulos.push(value)
-    // });
-    // console.log(capitulos);
-
-    // alternativa chatGPT
     let capitulos = []
     for (const item of this._dataIngreso) {
       const value = {
@@ -141,9 +138,9 @@ export class IndiceComponent implements OnInit {
       this._dataIngreso[7].value +
       this._dataIngreso[8].value
     );
-    /* #endregion */
+  }
 
-    /* #region  Total general ingresos para datos tabla */
+  async calcTotalesPresupuestoIngresos() {
     const totalPresupuestoIngresos = this._dataIngreso.reduce((acc, curr) => {
       Object.keys(curr).forEach((key, index) => {
         if (!acc[key]) {
@@ -156,25 +153,22 @@ export class IndiceComponent implements OnInit {
 
     this.totalPresupuestoIngresos = totalPresupuestoIngresos.value.toLocaleString();
     this.totalEjecutadoIngresos = totalPresupuestoIngresos.recaudado.toLocaleString();
-    /* #endregion */
+  }
 
-    /* #region politicas de gasto  */
-    // this._dataTable = await this._tableService.loadData('gastosProgramaPoliticas');
-    // this._dataGasto = this._dataTable.rowDataGastos;
+  async calcSumPoliticasGastos() {
     this._dataGasto = this._dataStoreService.dataTable.rowDataGastos;
 
-    let politicas = []
     for (const item of this._dataGasto) {
       const value = {
         name: `${item.CodPro}-${item.DesPro}`,
         value: item.Definitivas2022,
         recaudado: item.Pagos2022
       };
-      politicas.push(value);
+      this._politicasGastos.push(value);
     }
 
     // Totalizo por politicas de gasto
-    this._dataGasto = politicas.reduce((acc, curr) => {
+    this._politicasGastos = this._politicasGastos.reduce((acc, curr) => {
       const index = acc.findIndex(item => item.name === curr.name)
       index > -1 ? (acc[index].value += curr.value, acc[index].recaudado += curr.recaudado) : acc.push({
         name: curr.name,
@@ -183,33 +177,35 @@ export class IndiceComponent implements OnInit {
       })
       return acc
     }, [])
-    /* #endregion */
 
-    /* #region  gastosEconomicaCapitulos */
+  }
+
+  async calcSumGastos() {
     // Creo array de capitulos de gasto
-    let capitulosGastos = [];
     for (const item of this._dataGasto) {
       const value = {
         name: `${item.CodCap}-${item.DesCap}`,
-        value: item.Definitivas2022
+        value: item.Definitivas2022,
+        recaudado: item.Pagos2022
       };
-      capitulosGastos.push(value);
+      this._capitulosGastos.push(value);
     }
 
     // Totalizo por capitulo
-    capitulosGastos = this._dataGasto.reduce((acc, curr) => {
+    this._capitulosGastos = this._capitulosGastos.reduce((acc, curr) => {
       const index = acc.findIndex(item => item.name === curr.name)
-      index > -1 ? (acc[index].value += curr.value) : acc.push({
+      index > -1 ? (acc[index].value += curr.value, acc[index].recaudado += curr.recaudado) : acc.push({
         name: curr.name,
         value: curr.value,
+        recaudado: curr.recaudado
       })
       return acc
     }, [])
-    // console.log(capitulosGastos);
-    /* #endregion */
 
-    /* #region  Total general gastos para datos tabla */
-    const totalPresupuestoGastos = this._dataGasto.reduce((acc, curr) => {
+  }
+
+  async calcTotalesPresupuestoGastos() {
+    const totalPresupuestoGastos = this._capitulosGastos.reduce((acc, curr) => {
       Object.keys(curr).forEach((key, index) => {
         if (!acc[key]) {
           acc[key] = 0
@@ -218,52 +214,49 @@ export class IndiceComponent implements OnInit {
       })
       return acc
     }, {})
-    /* #endregion */
 
-    /* #region  Calculos valores para mostrar en tabla */
     this.noFinancieroGastos = (
-      this._dataGasto[0].value +
-      this._dataGasto[1].value +
-      this._dataGasto[2].value +
-      this._dataGasto[3].value +
-      this._dataGasto[4].value +
-      this._dataGasto[5].value +
-      this._dataGasto[6].value
+      this._capitulosGastos[0].value +
+      this._capitulosGastos[1].value +
+      this._capitulosGastos[2].value +
+      this._capitulosGastos[3].value +
+      this._capitulosGastos[4].value +
+      this._capitulosGastos[5].value +
+      this._capitulosGastos[6].value
     ).toLocaleString();
 
     this.corrientesGastos = (
-      this._dataGasto[0].value +
-      this._dataGasto[1].value +
-      this._dataGasto[2].value +
-      this._dataGasto[3].value +
-      this._dataGasto[4].value
+      this._capitulosGastos[0].value +
+      this._capitulosGastos[1].value +
+      this._capitulosGastos[2].value +
+      this._capitulosGastos[3].value +
+      this._capitulosGastos[4].value
     );
 
     this.capitalGastos = (
-      this._dataGasto[5].value +
-      this._dataGasto[6].value
+      this._capitulosGastos[5].value +
+      this._capitulosGastos[6].value
     ).toLocaleString();
 
     this.financierosGastos = (
-      this._dataGasto[7].value +
-      this._dataGasto[8].value
+      this._capitulosGastos[7].value +
+      this._capitulosGastos[8].value
     );
 
     this.totalPresupuestoGastos = totalPresupuestoGastos.value.toLocaleString();
     this.totalEjecutadoGastos = totalPresupuestoGastos.recaudado.toLocaleString();
+  }
 
+  async calcIndicadores() {
     this.ahorroBruto = (this.corrientesIngresos - this.corrientesGastos).toLocaleString();
 
     // Tengo que sumar capitulo 9 de gastos
-    this.ahorroNeto = ((this.corrientesIngresos - this.corrientesGastos) - capitulosGastos[7].value).toLocaleString();
+    this.ahorroNeto = ((this.corrientesIngresos - this.corrientesGastos) - this._capitulosGastos[7].value).toLocaleString();
     this.corrientesIngresos = this.corrientesIngresos.toLocaleString();
     this.corrientesGastos = this.corrientesGastos.toLocaleString();
     this.capacidadFinanciacion = (this.financierosIngresos - this.financierosGastos).toLocaleString();
     this.financierosIngresos = this.financierosIngresos.toLocaleString();
     this.financierosGastos = this.financierosGastos.toLocaleString();
-    /* #endregion */
-
-    this.showGraph()
   }
 
   showGraph() {
@@ -364,26 +357,36 @@ export class IndiceComponent implements OnInit {
           ],
           keys: ["from", "to", "weight"],
           data: [
-            ["Presupuesto", this._dataGasto[0].name, this._dataGasto[0].value],
-            ["Presupuesto", this._dataGasto[1].name, this._dataGasto[1].value],
-            ["Presupuesto", this._dataGasto[2].name, this._dataGasto[2].value],
-            ["Presupuesto", this._dataGasto[3].name, this._dataGasto[3].value],
-            ["Presupuesto", this._dataGasto[4].name, this._dataGasto[4].value],
-            ["Presupuesto", this._dataGasto[5].name, this._dataGasto[5].value],
-            ["Presupuesto", this._dataGasto[6].name, this._dataGasto[6].value],
-            ["Presupuesto", this._dataGasto[7].name, this._dataGasto[7].value],
-            ["Presupuesto", this._dataGasto[8].name, this._dataGasto[8].value],
-            ["Presupuesto", this._dataGasto[9].name, this._dataGasto[9].value],
-            ["Presupuesto", this._dataGasto[10].name, this._dataGasto[10].value],
-            ["Presupuesto", this._dataGasto[11].name, this._dataGasto[11].value],
-            ["Presupuesto", this._dataGasto[12].name, this._dataGasto[12].value],
-            ["Presupuesto", this._dataGasto[13].name, this._dataGasto[13].value],
-            ["Presupuesto", this._dataGasto[14].name, this._dataGasto[14].value],
-            ["Presupuesto", this._dataGasto[15].name, this._dataGasto[15].value],
-            ["Presupuesto", this._dataGasto[16].name, this._dataGasto[16].value],
-            ["Presupuesto", this._dataGasto[17].name, this._dataGasto[17].value],
-            ["Presupuesto", this._dataGasto[18].name, this._dataGasto[18].value],
-            ["Presupuesto", this._dataGasto[19].name, this._dataGasto[19].value]
+            ["Presupuesto", this._capitulosGastos[0].name, this._capitulosGastos[0].value],
+            ["Presupuesto", this._capitulosGastos[1].name, this._capitulosGastos[1].value],
+            ["Presupuesto", this._capitulosGastos[2].name, this._capitulosGastos[2].value],
+            ["Presupuesto", this._capitulosGastos[3].name, this._capitulosGastos[3].value],
+            ["Presupuesto", this._capitulosGastos[4].name, this._capitulosGastos[4].value],
+            ["Presupuesto", this._capitulosGastos[5].name, this._capitulosGastos[5].value],
+            ["Presupuesto", this._capitulosGastos[6].name, this._capitulosGastos[6].value],
+            ["Presupuesto", this._capitulosGastos[7].name, this._capitulosGastos[7].value],
+            ["Presupuesto", this._capitulosGastos[8].name, this._capitulosGastos[8].value],
+
+            // ["Presupuesto", this._politicasGastos[0].name, this._politicasGastos[0].value],
+            // ["Presupuesto", this._politicasGastos[1].name, this._politicasGastos[1].value],
+            // ["Presupuesto", this._politicasGastos[2].name, this._politicasGastos[2].value],
+            // ["Presupuesto", this._politicasGastos[3].name, this._politicasGastos[3].value],
+            // ["Presupuesto", this._politicasGastos[4].name, this._politicasGastos[4].value],
+            // ["Presupuesto", this._politicasGastos[5].name, this._politicasGastos[5].value],
+            // ["Presupuesto", this._politicasGastos[6].name, this._politicasGastos[6].value],
+            // ["Presupuesto", this._politicasGastos[7].name, this._politicasGastos[7].value],
+            // ["Presupuesto", this._politicasGastos[8].name, this._politicasGastos[8].value],
+            // ["Presupuesto", this._politicasGastos[9].name, this._politicasGastos[9].value],
+            // ["Presupuesto", this._politicasGastos[10].name, this._politicasGastos[10].value],
+            // ["Presupuesto", this._politicasGastos[11].name, this._politicasGastos[11].value],
+            // ["Presupuesto", this._politicasGastos[12].name, this._politicasGastos[12].value],
+            // ["Presupuesto", this._politicasGastos[13].name, this._politicasGastos[13].value],
+            // ["Presupuesto", this._politicasGastos[14].name, this._politicasGastos[14].value],
+            // ["Presupuesto", this._politicasGastos[15].name, this._politicasGastos[15].value],
+            // ["Presupuesto", this._politicasGastos[16].name, this._politicasGastos[16].value],
+            // ["Presupuesto", this._politicasGastos[17].name, this._politicasGastos[17].value],
+            // ["Presupuesto", this._politicasGastos[18].name, this._politicasGastos[18].value],
+            // ["Presupuesto", this._politicasGastos[19].name, this._politicasGastos[19].value]
           ],
           type: "sankey",
           name: "",
