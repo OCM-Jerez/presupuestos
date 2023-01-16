abefaa57207112d4a1af7a9b4099b4f30ed625d4
mamjerez@gmail.com <mamjerez@gmail.com>
2023-01-16 20:38:28 +0100

    Inicializar a true para que se vea table-gastos

--- src/app/services/hasDataChange.service.ts
+++ src/app/services/hasDataChange.service.ts
@@ -5,7 +5,7 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class HasDataChangeService {
-  private HasDataChangeSource = new BehaviorSubject<boolean>(false);
+  private HasDataChangeSource = new BehaviorSubject<boolean>(true);
   currentHasDataChange = this.HasDataChangeSource.asObservable();
   change(hasDataChange: boolean) { this.HasDataChangeSource.next(hasDataChange) };
 }
