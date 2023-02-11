83c7d3d82a6e7710b9e7477cb3a117032fd67761
mamjerez@gmail.com <mamjerez@gmail.com>
2023-02-11 19:30:21 +0100

    Clean code

--- src/app/services/tabState.service.ts
+++ src/app/services/tabState.service.ts
@@ -1,53 +1,31 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class TabStateService {
-    // private tabState = {
-    //     tab1: {
-    //         buttonName: 'Por programa'
-    //     },
-    //     tab2: {
-    //         buttonName: 'Por política'
-    //     },
-    //     tab3: {
-    //         buttonName: 'Por artículo'
-    //     },
-    //     tab4: {
-    //         buttonName: 'Por artículo'
-    //     }
-    // };
-
-    private tabState: IConfigTag[] = [{
-        tabSelected: 'tab1',
-        subTabSelected: 'por capitulo',
-        optionRadioSelected: 'presupuestado'
-    },
-    {
-        tabSelected: 'tab2',
-        subTabSelected: 'Por política',
-    },
-    {
-        tabSelected: 'tab3',
-    },
-    {
-        tabSelected: 'tab4',
-        subTabSelected: 'Por artículo',
-    }
-
+    private tabState: IConfigTag[] = [
+        {
+            tabSelected: 'tab1',
+            subTabSelected: 'por capitulo',
+            optionRadioSelected: 'presupuestado'
+        },
+        {
+            tabSelected: 'tab2',
+            subTabSelected: 'Por política',
+        },
+        {
+            tabSelected: 'tab3',
+        },
+        {
+            tabSelected: 'tab4',
+            subTabSelected: 'Por artículo',
+        }
     ]
 
     constructor() { }
 
     setTabState(tabName: string, subTabSelected: string, optionRadioSelected?: string) {
-        //console.log('setTabState', tabName, buttonName);
-
-        // if (!this.tabState[tabName]) {
-        //     this.tabState[tabName] = {};
-        // }
-        //this.tabState[0].subTabSelected = subTabSelected;
         const tab = this.tabState.find((tab) => tab.tabSelected === tabName);
         tab.subTabSelected = subTabSelected;
         if (tab.optionRadioSelected) {
