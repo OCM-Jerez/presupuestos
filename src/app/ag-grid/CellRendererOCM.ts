e9ea07f5bae657719304039a1417d717747725a7
mamjerez@gmail.com <mamjerez@gmail.com>
2023-03-04 12:54:04 +0100

    Change font-family

--- src/app/ag-grid/CellRendererOCM.ts
+++ src/app/ag-grid/CellRendererOCM.ts
@@ -13,12 +13,12 @@ export function CellRendererOCM(params: any) {
         case 0:  // Total primer nivel.
           return `<p style="text-align: right; color: red; font-size: 13px; margin: 0px;font-weight: bold">${valorFormateado}</p>`;
         case -1: // Total general.
-          return `<p style="text-align: right; color: red; font-size: 14px; margin: 0px;font-weight: bold">${valorFormateado}</p>`;
+          return `<p style="text-align: right; color: red; font-size: 20px; font-family:roboto;margin: 0px;font-weight: bold">${valorFormateado}</p>`;
         default:
           return 'SIN FORMATO';
       }
     } else {
-      return `<p style="font-size: 12px; text-align: right; margin: 0px;">${valorFormateado}</p>`;
+      return `<p style="font-size: 16px; font-family:roboto;text-align: right; margin: 0px;">${valorFormateado}</p>`;
     }
   } else {
     return '';
@@ -27,6 +27,12 @@ export function CellRendererOCM(params: any) {
 
 
 export function CellRendererOCMtext(params: any) {
-  return params.value
+  // return params.value
+  if (params.value) {
+    return `<p style="font-size: 18px; font-family:roboto; text-align: left; margin: 0px;">${params.value}</p>`
+  } else {
+    return '';
+  }
 }
+
 
