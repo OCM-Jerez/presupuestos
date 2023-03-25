04aab2dc8117adc1cf186de9a2b498859eb800ba
mamjerez@gmail.com <mamjerez@gmail.com>
2023-03-25 18:44:20 +0100

    Use example interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,93 +1,102 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
 import { environment } from '../../environments/environment';
 
 import { TableService } from '../services/table.service';
 
 import { IDataTable } from '../commons/interfaces/dataTable.interface';
+import { IExample } from '../commons/interfaces/example.interface';
+import { ITablaAleatoria } from '../commons/interfaces/tablaAleatoria.interface';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  private _dataTable: IDataTable;
-  textoTabla: string;
-  liqDate = environment.liqDate2023;
-  examples: { name: string; value: number }[] = [
-    { name: '', value: 0 },
-    { name: '', value: 0 },
-    { name: '', value: 0 },
-  ];
-  dataTablaAleatoria: any[] = [];
+    private _dataTable: IDataTable;
+    private _dataTablaAleatoria: ITablaAleatoria[] = [];
+    textoTabla: string;
+    liqDate = environment.liqDate2023;
+    examples: IExample[] = Array.from({ length: 3 }, () => ({
+        name: '',
+        value: 0,
+    }));
 
-  constructor(
-    private _tableService: TableService,
-    private _router: Router
-  ) { }
+    constructor(private _tableService: TableService, private _router: Router) {}
 
-  ngOnInit(): void {
-    this._randomData()
-  }
-
-  private async _randomData(): Promise<void> {
-    this._dataTable = await this._tableService.loadDataInitial();
-
-    const ingresoGasto = (Math.random() >= 0.5) ? true : false;
-    ingresoGasto
-      ? this.dataTablaAleatoria = await this.getData('DesEco', 'DerechosReconocidosNetos2023', this._dataTable.rowDataIngresos)
-      : this.dataTablaAleatoria = await this.getData('DesOrg', 'Pagos2023', this._dataTable.rowDataGastos);
-    this.textoTabla = ingresoGasto ? '¿Cuánto ha recaudado el Ayuntamiento por...?' : '¿Cuánto ha gastado la delegación de...?';
-    await this.fillDatosAleatorios();
-  }
-
-  async getData(name: string, value: string, data: any[] = []) {
-
-    const resultado = data.reduce((acc, curr) => {
-      const itemName = curr[name];
-      const itemValue = curr[value];
-
-      const itemEncontrado = acc.find(item => item.name === itemName);
-      if (itemEncontrado) {
-        itemEncontrado.value += itemValue;
-      } else {
-        acc.push({
-          name: itemName,
-          value: itemValue,
-        });
-      }
-
-      return acc;
-    }, []);
-    return resultado;
-  }
-
-  async fillDatosAleatorios() {
-    this.dataTablaAleatoria.sort(() => Math.random() - 0.5);
-
-    for (let i = 0; i < 3; i++) {
-      this.examples[i].name = this.dataTablaAleatoria[i].name;
-      this.examples[i].value = this.dataTablaAleatoria[i].value.toLocaleString("de-DE");
+    ngOnInit(): void {
+        this._randomData();
     }
-  }
 
-  visionGlobal() {
-    this._router.navigateByUrl('/home')
-  }
+    private async _randomData(): Promise<void> {
+        this._dataTable = await this._tableService.loadDataInitial();
 
-  detallePresupuesto() {
-    this._router.navigateByUrl('/detallePresupuesto')
-  }
+        const ingresoGasto = Math.random() >= 0.5 ? true : false;
+        ingresoGasto
+            ? (this._dataTablaAleatoria = await this.getData(
+                  'DesEco',
+                  'DerechosReconocidosNetos2023',
+                  this._dataTable.rowDataIngresos
+              ))
+            : (this._dataTablaAleatoria = await this.getData(
+                  'DesOrg',
+                  'Pagos2023',
+                  this._dataTable.rowDataGastos
+              ));
 
-  licitaciones() {
-    window.open('https://con.ocmjerez.org/', '_blank');
-  }
+        this.textoTabla = ingresoGasto
+            ? '¿Cuánto ha recaudado el Ayuntamiento por...?'
+            : '¿Cuánto ha gastado la delegación de...?';
 
-  empleados() {
-    this._router.navigateByUrl('/empleados')
-  }
+        await this.fillDatosAleatorios();
+    }
 
+    async getData(name: string, value: string, data: any[] = []) {
+        const resultado = data.reduce((acc, curr) => {
+            const itemName = curr[name];
+            const itemValue = curr[value];
+            const itemEncontrado = acc.find((item) => item.name === itemName);
+
+            if (itemEncontrado) {
+                itemEncontrado.value += itemValue;
+            } else {
+                acc.push({
+                    name: itemName,
+                    value: itemValue,
+                });
+            }
+
+            return acc;
+        }, []);
+
+        return resultado;
+    }
+
+    async fillDatosAleatorios() {
+        this._dataTablaAleatoria.sort(() => Math.random() - 0.5);
+
+        for (let i = 0; i < 3; i++) {
+            this.examples[i].name = this._dataTablaAleatoria[i].name;
+            this.examples[i].value =
+                this._dataTablaAleatoria[i].value.toLocaleString('de-DE');
+        }
+    }
+
+    visionGlobal() {
+        this._router.navigateByUrl('/home');
+    }
+
+    detallePresupuesto() {
+        this._router.navigateByUrl('/detallePresupuesto');
+    }
+
+    licitaciones() {
+        window.open('https://con.ocmjerez.org/', '_blank');
+    }
+
+    empleados() {
+        this._router.navigateByUrl('/empleados');
+    }
 }
-
-
