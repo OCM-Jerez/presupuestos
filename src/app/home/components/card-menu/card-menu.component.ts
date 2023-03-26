a89ff2e946ed14962959cedd0e97e1ad75c19620
mamjerez@gmail.com <mamjerez@gmail.com>
2023-03-26 08:24:16 +0200

    Delete unused code

--- src/app/home/components/card-menu/card-menu.component.ts
+++ src/app/home/components/card-menu/card-menu.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-card-menu',
@@ -11,22 +10,4 @@ export class CardMenuComponent {
     @Input() titulo: string;
     @Input() subtitulo: string;
     @Input() textButton: string;
-
-    constructor(private _router: Router) {}
-
-    visionGlobal() {
-        this._router.navigateByUrl('/home');
-    }
-
-    detallePresupuesto() {
-        this._router.navigateByUrl('/detallePresupuesto');
-    }
-
-    licitaciones() {
-        window.open('https://con.ocmjerez.org/', '_blank');
-    }
-
-    empleados() {
-        this._router.navigateByUrl('/empleados');
-    }
 }
