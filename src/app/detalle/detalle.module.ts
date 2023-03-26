149002655796bb056896ae222538d3bedb6182e2
mamjerez@gmail.com <mamjerez@gmail.com>
2023-03-26 06:32:43 +0200

    Delete NgSelectModule

--- src/app/detalle/detalle.module.ts
+++ src/app/detalle/detalle.module.ts
@@ -5,35 +5,32 @@ import { RouterModule, Routes } from '@angular/router';
 import { AgGridModule } from 'ag-grid-angular';
 import { HighchartsChartModule } from 'highcharts-angular';
 
-import { DetalleComponent } from './detalle.component';
-import { TableIngresosModule } from '../tables/table-ingresos/table-ingresos.module';
+import { FormsModule } from '@angular/forms';
 import { CheckboxModule } from '../commons/components/checkbox/checkbox.module';
 import { GastosModule } from '../tables/gastos/gastos.module';
-// import { NgSelectModule } from '@ng-select/ng-select';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from '../app.component';
-import { TablePresupuestoComponent } from './components/table-presupuesto/table-presupuesto.component';
+import { TableIngresosModule } from '../tables/table-ingresos/table-ingresos.module';
 import { ClasificationTabsComponent } from './components/clasification-tabs/clasification-tabs.component';
 import { BarClasificationTabsComponent } from './components/clasification-tabs/components/bar-clasification-tabs/bar-clasification-tabs.component';
+import { TablePresupuestoComponent } from './components/table-presupuesto/table-presupuesto.component';
+import { DetalleComponent } from './detalle.component';
 
-export const routes: Routes = [{ path: '', component: DetalleComponent }]
+export const routes: Routes = [{ path: '', component: DetalleComponent }];
 @NgModule({
-  declarations: [
-    DetalleComponent,
-    TablePresupuestoComponent,
-    ClasificationTabsComponent,
-    BarClasificationTabsComponent
-  ],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes),
-    AgGridModule,
-    HighchartsChartModule,
-    TableIngresosModule,
-    CheckboxModule,
-    GastosModule,
-    FormsModule,
-    // NgSelectModule
-  ],
+    declarations: [
+        DetalleComponent,
+        TablePresupuestoComponent,
+        ClasificationTabsComponent,
+        BarClasificationTabsComponent,
+    ],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+        AgGridModule,
+        HighchartsChartModule,
+        TableIngresosModule,
+        CheckboxModule,
+        GastosModule,
+        FormsModule,
+    ],
 })
-export class DetallePresupuestoModule { }
+export class DetallePresupuestoModule {}
