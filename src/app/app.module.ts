71fee6134a5c75616a02175bc9d322df0995969f
mamjerez@gmail.com <mamjerez@gmail.com>
2023-03-26 06:28:40 +0200

    Delete NgSelectModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
-// import { NgSelectModule } from '@ng-select/ng-select';
 import 'ag-grid-enterprise';
 
 // Rutas
 import { AppRoutingModule } from './app-routing.module';
 
 // Components
+import { HeaderAgGridComponent } from './ag-grid/header-ag-grid/header-ag-grid.component';
 import { AppComponent } from './app.component';
+import { CheckboxModule } from './commons/components/checkbox/checkbox.module';
+import { DetallePresupuestoModule } from './detalle/detalle.module';
+import { IndiceComponent } from './indice/indice.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
-import { IndiceComponent } from './indice/indice.component';
 import { AvalaibleYearsService } from './services/avalaibleYears.service';
-import { HeaderAgGridComponent } from './ag-grid/header-ag-grid/header-ag-grid.component';
-import { DetallePresupuestoModule } from './detalle/detalle.module';
-import { CheckboxModule } from './commons/components/checkbox/checkbox.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent,
-    NavbarComponent,
-    IndiceComponent,
-    HeaderAgGridComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    FormsModule,
-    // NgSelectModule,
-    ReactiveFormsModule,
-    CheckboxModule
-  ],
+    declarations: [
+        AppComponent,
+        FooterComponent,
+        NavbarComponent,
+        IndiceComponent,
+        HeaderAgGridComponent,
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        CheckboxModule,
+        DetallePresupuestoModule,
+    ],
 
-  providers: [
-    AvalaibleYearsService
-  ],
-  bootstrap: [AppComponent]
+    providers: [AvalaibleYearsService],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
