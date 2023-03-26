8d67c6caf1d25cd005b91fbdded1e91ea3e45f6f
mamjerez@gmail.com <mamjerez@gmail.com>
2023-03-26 06:33:51 +0200

    Clean code

--- src/app/empleados/empleados.module.ts
+++ src/app/empleados/empleados.module.ts
@@ -1,15 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
 import { EmpleadosComponent } from './empleados.component';
 
-
-
 @NgModule({
-  declarations: [
-    EmpleadosComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+    declarations: [EmpleadosComponent],
+    imports: [CommonModule],
 })
-export class EmpleadosModule { }
+export class EmpleadosModule {}
