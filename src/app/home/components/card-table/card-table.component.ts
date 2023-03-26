d62512c81ba95f1a04998ecac07f1fdcd19523df
mamjerez@gmail.com <mamjerez@gmail.com>
2023-03-26 07:39:38 +0200

    Add  <app-card-table></app-card-table>

--- src/app/home/components/card-table/card-table.component.ts
+++ src/app/home/components/card-table/card-table.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { environment } from '../../../../environments/environment';
+import { IDataTable } from '../../../commons/interfaces/dataTable.interface';
+import { IExample } from '../../../commons/interfaces/example.interface';
+import { ITablaAleatoria } from '../../../commons/interfaces/tablaAleatoria.interface';
+import { TableService } from '../../../services/table.service';
 
 @Component({
-  selector: 'app-card-table',
-  templateUrl: './card-table.component.html',
-  styleUrls: ['./card-table.component.scss']
+    selector: 'app-card-table',
+    templateUrl: './card-table.component.html',
+    styleUrls: ['./card-table.component.scss'],
 })
-export class CardTableComponent {
+export class CardTableComponent implements OnInit {
+    private _dataTable: IDataTable;
+    private _dataTablaAleatoria: ITablaAleatoria[] = [];
+    textoTabla: string;
+    liqDate = environment.liqDate2023;
+    examples: IExample[] = Array.from({ length: 3 }, () => ({
+        name: '',
+        value: 0,
+    }));
 
+    constructor(private _tableService: TableService, private _router: Router) {}
+
+    ngOnInit(): void {
+        this._randomData();
+    }
+
+    private async _randomData(): Promise<void> {
+        this._dataTable = await this._tableService.loadDataInitial();
+
+        const ingresoGasto = Math.random() >= 0.5 ? true : false;
+        ingresoGasto
+            ? (this._dataTablaAleatoria = await this.getData(
+                  'DesEco',
+                  'DerechosReconocidosNetos2023',
+                  this._dataTable.rowDataIngresos
+              ))
+            : (this._dataTablaAleatoria = await this.getData(
+                  'DesOrg',
+                  'Pagos2023',
+                  this._dataTable.rowDataGastos
+              ));
+
+        this.textoTabla = ingresoGasto
+            ? '¿Cuánto ha recaudado el Ayuntamiento por...?'
+            : '¿Cuánto ha gastado la delegación de...?';
+
+        await this.fillDatosAleatorios();
+    }
+
+    async getData(name: string, value: string, data: any[] = []) {
+        const resultado = data.reduce((acc, curr) => {
+            const itemName = curr[name];
+            const itemValue = curr[value];
+            const itemEncontrado = acc.find((item) => item.name === itemName);
+
+            if (itemEncontrado) {
+                itemEncontrado.value += itemValue;
+            } else {
+                acc.push({
+                    name: itemName,
+                    value: itemValue,
+                });
+            }
+
+            return acc;
+        }, []);
+
+        return resultado;
+    }
+
+    async fillDatosAleatorios() {
+        this._dataTablaAleatoria.sort(() => Math.random() - 0.5);
+
+        for (let i = 0; i < 3; i++) {
+            this.examples[i].name = this._dataTablaAleatoria[i].name;
+            this.examples[i].value =
+                this._dataTablaAleatoria[i].value.toLocaleString('de-DE');
+        }
+    }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { CardInfoComponent } from './components/card-info/card-info.component';
+import { CardTableComponent } from './components/card-table/card-table.component';
 import { HomeComponent } from './home.component';
 
 @NgModule({
-    declarations: [HomeComponent, CardInfoComponent],
+    declarations: [HomeComponent, CardInfoComponent, CardTableComponent],
     imports: [CommonModule],
 })
 export class HomeModule {}
