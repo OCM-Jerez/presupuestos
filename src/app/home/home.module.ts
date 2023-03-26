c7ac534b69bbc03908750a0c8e8af1e83b81b901
mamjerez@gmail.com <mamjerez@gmail.com>
2023-03-26 07:03:40 +0200

    Clean code

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { CardInfoComponent } from './components/card-info/card-info.component';
-
 import { HomeComponent } from './home.component';
 
 @NgModule({
