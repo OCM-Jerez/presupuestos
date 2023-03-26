2247453e4b6e400eadece662c5c15935f9afee12
mamjerez@gmail.com <mamjerez@gmail.com>
2023-03-26 08:59:08 +0200

    Try use   cardMenus = [] NO FUNCIONA

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,41 @@ import { Router } from '@angular/router';
     styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+    cardMenus = [
+        {
+            rutaImagen: '../../assets/img/home/menu1-400x250.webp',
+            titulo: 'Visión global',
+            subtitulo:
+                'Para que tengas una idea general de los ingresos y gastos del Ayuntamiento de Jerez.',
+            funcion: this.visionGlobal,
+            textButton: 'Visión global',
+        },
+        {
+            rutaImagen: '../../assets/img/home/menu2-400x250.webp',
+            titulo: 'Detalle del presupuesto',
+            subtitulo:
+                'Educación, protección y bienestar social, cultura... ¿Qué área te interesa?',
+            funcion: this.detallePresupuesto,
+            textButton: 'Detalle',
+        },
+        {
+            rutaImagen: '../../assets/img/home/menu3-400x250.webp',
+            titulo: 'Licitaciones',
+            subtitulo:
+                'Todos las licitaciones de obras, contratos menores de nuestro Ayuntamiento',
+            funcion: this.licitaciones,
+            textButton: 'Licitaciones',
+        },
+        {
+            rutaImagen: '../../assets/img/home/menu4-400x250.webp',
+            titulo: 'Empleados municipales',
+            subtitulo:
+                'Información sobre los empleados de nuestro Ayuntamiento',
+            funcion: this.empleados,
+            textButton: 'Empleados',
+        },
+    ];
+
     constructor(private _router: Router) {}
 
     visionGlobal() {
