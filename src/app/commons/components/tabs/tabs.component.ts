bf869f117d1e359afdfd477033c256fe0a7a7752
mamjerez@gmail.com <mamjerez@gmail.com>
2023-03-28 21:16:31 +0200

    Clean code

--- src/app/commons/components/tabs/tabs.component.ts
+++ src/app/commons/components/tabs/tabs.component.ts
@@ -6,6 +6,7 @@ import {
     Output,
     QueryList,
 } from '@angular/core';
+
 import { TabComponent } from './tab/tab.component';
 
 @Component({
@@ -18,6 +19,7 @@ export class TabsComponent implements AfterContentInit {
     @Output() selectedTab = new EventEmitter();
 
     ngAfterContentInit(): void {
+        // console.log(this.tabs);
         const activeTabs = this.tabs.filter((tab) => tab.active);
 
         if (activeTabs.length === 0) {
@@ -28,12 +30,17 @@ export class TabsComponent implements AfterContentInit {
 
     selectTab(tab: TabComponent): void {
         const tabs = this.tabs.toArray();
+        // console.log(tabs);
         const tabActive = tabs.find((tab) => tab.active);
 
         if (tabActive && tabActive.idTab !== tab.idTab) {
             this.tabs.toArray().forEach((tab) => (tab.active = false));
             tab.active = true;
+
             this.selectedTab.emit(tab.idTab);
+            // this.selectedTab.subscribe((data) => {
+            //     console.log('Contenido del evento:', data);
+            // });
         }
     }
 }
