01587375463a6c374a81938250e41a7f169e193c
mamjerez@gmail.com <mamjerez@gmail.com>
2023-03-28 21:18:38 +0200

    Clean code

--- src/app/commons/components/tabs/tab/tab.component.ts
+++ src/app/commons/components/tabs/tab/tab.component.ts
@@ -1,12 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component, Input } from '@angular/core';
 
 @Component({
-	selector: 'app-tab',
-	templateUrl: './tab.component.html'
+    selector: 'app-tab',
+    templateUrl: './tab.component.html',
 })
 export class TabComponent {
-	@Input() tabTitle!: string;
-	@Input() active = false;
-	@Input() idTab!: number;
+    @Input() tabTitle!: string;
+    @Input() active = false;
+    @Input() idTab!: number;
 }
