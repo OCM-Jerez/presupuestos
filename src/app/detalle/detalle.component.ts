1c862741db17d83c2b09787a275bad70092bb904
mamjerez@gmail.com <mamjerez@gmail.com>
2023-04-01 20:10:40 +0200

    Delete code for switch

--- src/app/detalle/detalle.component.ts
+++ src/app/detalle/detalle.component.ts
@@ -11,22 +11,25 @@ import { CLASIFICATION_TYPE } from '../commons/util/util';
     styleUrls: ['./detalle.component.scss'],
 })
 export class DetalleComponent {
+    dataTreeMap: any;
+    private _typeClasification: CLASIFICATION_TYPE;
+    private readonly _tabMappings: { [id: number]: { code: string; desc: string } } = {
+        1: { code: 'CodEco', desc: 'DesEco' },
+        2: { code: 'CodPro', desc: 'DesPro' },
+        3: { code: 'CodOrg', desc: 'DesOrg' },
+        4: { code: 'CodCap', desc: 'DesCap' },
+    };
+
     constructor(
         private _prepareDataTreemapService: PrepareDataTreemapService,
         private _tableService: TableService
     ) {}
 
-    private _typeClasification: CLASIFICATION_TYPE;
-    dataTreeMap: any;
-
     async hasChangeCheckbox() {
         let data = await this._tableService.loadData(this._typeClasification);
     }
 
-    // selectedIndex = 1;
-
     async selectedTab(idTab: number) {
-        // this.selectedIndex = idTab;
         const typeClasification = this.getTypeClasification(idTab);
         const loadData = await this._tableService.loadData(typeClasification);
 
@@ -49,63 +52,8 @@ export class DetalleComponent {
         return classificationTypes[idTab];
     }
 
-    private readonly _tabMappings: { [id: number]: { code: string; desc: string } } = {
-        1: { code: 'CodEco', desc: 'DesEco' },
-        2: { code: 'CodPro', desc: 'DesPro' },
-        3: { code: 'CodOrg', desc: 'DesOrg' },
-        4: { code: 'CodCap', desc: 'DesCap' },
-    };
-
     async dataTreemap(idTab: number, data: any) {
         const { code, desc } = this._tabMappings[idTab];
         return this._prepareDataTreemapService.calcSeries(data, code, desc, 'Definitivas2023');
     }
-
-    // getTypeClasification(idTab: number): CLASIFICATION_TYPE {
-    //     switch (idTab) {
-    //         case 1:
-    //             return 'ingresosEconomicaEconomicos';
-    //         case 2:
-    //             return 'gastosProgramaPoliticas';
-    //         case 3:
-    //             return 'gastosOrganicaOrganicos';
-    //         case 4:
-    //             return 'gastosEconomicaConceptos';
-    //     }
-    //     return 'ingresosEconomicaEconomicos';
-    // }
-
-    // async dataTreemap(idTab: number, data: any) {
-    //     // Datos para grafico
-    //     switch (idTab) {
-    //         case 1:
-    //             return await this._prepareDataTreemapService.calcSeries(
-    //                 data,
-    //                 'CodEco',
-    //                 'DesEco',
-    //                 'Definitivas2023'
-    //             );
-    //         case 2:
-    //             return await this._prepareDataTreemapService.calcSeries(
-    //                 data,
-    //                 'CodPro',
-    //                 'DesPro',
-    //                 'Definitivas2023'
-    //             );
-    //         case 3:
-    //             return await this._prepareDataTreemapService.calcSeries(
-    //                 data,
-    //                 'CodOrg',
-    //                 'DesOrg',
-    //                 'Definitivas2023'
-    //             );
-    //         case 4:
-    //             return await this._prepareDataTreemapService.calcSeries(
-    //                 data,
-    //                 'CodCap',
-    //                 'DesCap',
-    //                 'Definitivas2023'
-    //             );
-    //     }
-    // }
 }
