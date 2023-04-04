a738855e497a4f6ac320535adac66f8d569cbcfd
mamjerez@gmail.com <mamjerez@gmail.com>
2023-04-04 17:37:44 +0200

    No hace falta usar <app-clasification-tabs>

--- src/app/detalle/detalle.component.ts
+++ src/app/detalle/detalle.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CLASIFICATION_TYPE } from '../commons/util/util';
+import { PrepareDataTreemapService } from '../services/prepareDataTreemap.service';
+import { TableService } from '../services/table.service';
 
 @Component({
     selector: 'app-detalle-presupuesto',
@@ -6,49 +9,50 @@ import { Component } from '@angular/core';
     styleUrls: ['./detalle.component.scss'],
 })
 export class DetalleComponent {
-    // dataTreeMap: any;
-    // idTab: number;
-    // private _typeClasification: CLASIFICATION_TYPE;
-    // private readonly _tabMappings: { [id: number]: { code: string; desc: string } } = {
-    //     1: { code: 'CodEco', desc: 'DesEco' },
-    //     2: { code: 'CodPro', desc: 'DesPro' },
-    //     3: { code: 'CodOrg', desc: 'DesOrg' },
-    //     4: { code: 'CodCap', desc: 'DesCap' },
-    // };
+    dataTreeMap: any;
+    idTab: number;
+    private _typeClasification: CLASIFICATION_TYPE;
+    private readonly _tabMappings: { [id: number]: { code: string; desc: string } } = {
+        1: { code: 'CodEco', desc: 'DesEco' },
+        2: { code: 'CodPro', desc: 'DesPro' },
+        3: { code: 'CodOrg', desc: 'DesOrg' },
+        4: { code: 'CodCap', desc: 'DesCap' },
+    };
 
-    // constructor(
-    //     private _prepareDataTreemapService: PrepareDataTreemapService,
-    //     private _tableService: TableService
-    // ) {}
+    constructor(private _prepareDataTreemapService: PrepareDataTreemapService, private _tableService: TableService) {}
 
     async hasChangeCheckbox() {
-        // let data = await this._tableService.loadData(this._typeClasification);
+        let data = await this._tableService.loadData(this._typeClasification);
     }
 
     async selectedTab(idTab: number) {
-        //     const typeClasification = this.getTypeClasification(idTab);
-        //     const loadData = await this._tableService.loadData(typeClasification);
-        //     if (idTab === 1) {
-        //         this.dataTreeMap = await this.dataTreemap(idTab, loadData.rowDataIngresos);
-        //     } else {
-        //         this.dataTreeMap = await this.dataTreemap(idTab, loadData.rowDataGastos);
-        //     }
+        this.idTab = idTab;
+        const typeClasification = this.getTypeClasification(idTab);
+        const loadData = await this._tableService.loadData(typeClasification);
+        if (idTab === 1) {
+            this.dataTreeMap = await this.dataTreemap(idTab, loadData.rowDataIngresos);
+        } else {
+            this.dataTreeMap = await this.dataTreemap(idTab, loadData.rowDataGastos);
+        }
     }
 
-    // getTypeClasification(idTab: number): CLASIFICATION_TYPE {
-    //     const classificationTypes: {
-    //         [key: number]: CLASIFICATION_TYPE;
-    //     } = {
-    //         1: 'ingresosEconomicaEconomicos',
-    //         2: 'gastosProgramaPoliticas',
-    //         3: 'gastosOrganicaOrganicos',
-    //         4: 'gastosEconomicaConceptos',
-    //     };
-    //     return classificationTypes[idTab];
-    // }
+    // Determina el tipo de clasificación que se mostrar primero al cambiar de tab
+    getTypeClasification(idTab: number): CLASIFICATION_TYPE {
+        const classificationTypes: {
+            [key: number]: CLASIFICATION_TYPE;
+        } = {
+            1: 'ingresosEconomicaEconomicos',
+            2: 'gastosProgramaPoliticas',
+            3: 'gastosOrganicaOrganicos',
+            4: 'gastosEconomicaEconomicos',
+        };
+        return classificationTypes[idTab];
+    }
 
-    //     async dataTreemap(idTab: number, data: any) {
-    //         const { code, desc } = this._tabMappings[idTab];
-    //         return this._prepareDataTreemapService.calcSeries(data, code, desc, 'Definitivas2023');
-    //     }
+    async dataTreemap(idTab: number, data: any) {
+        console.log('idTab', idTab);
+
+        const { code, desc } = this._tabMappings[idTab];
+        return this._prepareDataTreemapService.calcSeries(data, code, desc, 'Definitivas2023');
+    }
 }
