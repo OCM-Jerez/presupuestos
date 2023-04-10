e0b0bd9404d175a6b9007de48aa415d3c32bbeb5
mamjerez@gmail.com <mamjerez@gmail.com>
2023-04-10 21:58:17 +0200

    Delete export const routes: Routes =

--- src/app/detalle/detalle.module.ts
+++ src/app/detalle/detalle.module.ts
@@ -1,31 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AgGridModule } from 'ag-grid-angular';
 import { HighchartsChartModule } from 'highcharts-angular';
 
-import { FormsModule } from '@angular/forms';
 import { CheckboxModule } from '../commons/components/checkbox/checkbox.module';
 import { GastosModule } from '../tables/gastos/gastos.module';
 import { TableIngresosModule } from '../tables/table-ingresos/table-ingresos.module';
-import { TablePresupuestoComponent } from './components/table-presupuesto/table-presupuesto.component';
 import { TabsModule } from './components/tabs/tabs.module';
 import { TreemapModule } from './components/treemap/treemap.module';
+
+import { TablePresupuestoComponent } from './components/table-presupuesto/table-presupuesto.component';
 import { DetalleComponent } from './detalle.component';
 
-export const routes: Routes = [{ path: '', component: DetalleComponent }];
+// export const routes: Routes = [{ path: '', component: DetalleComponent }];
 @NgModule({
     declarations: [DetalleComponent, TablePresupuestoComponent],
     imports: [
         CommonModule,
-        RouterModule.forChild(routes),
+        FormsModule,
+        // RouterModule.forChild(routes),
         AgGridModule,
         HighchartsChartModule,
-        TableIngresosModule,
         CheckboxModule,
         GastosModule,
-        FormsModule,
+        TableIngresosModule,
         TabsModule,
         TreemapModule,
     ],
