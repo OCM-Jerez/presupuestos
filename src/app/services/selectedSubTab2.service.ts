15662c4238018f02dd808e95d2ab1c90af89596b
mamjerez@gmail.com <mamjerez@gmail.com>
2023-04-15 15:23:57 +0200

    Create

---
+++ src/app/services/selectedSubTab2.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class SelectedSubTab2Service {
+    private SelectedSubTab2 = new BehaviorSubject<string>('Por programa');
+    source$ = this.SelectedSubTab2.asObservable();
+
+    setSelectedSubTab2(selectedSubTab2: string) {
+        this.SelectedSubTab2.next(selectedSubTab2);
+    }
+}
