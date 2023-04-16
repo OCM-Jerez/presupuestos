15b7d8ef76f5141570d8949bf319650685416ac5
mamjerez@gmail.com <mamjerez@gmail.com>
2023-04-16 15:00:35 +0200

    Falla al camabiar de tab

--- src/app/detalle/components/treemap/treemap.component.ts
+++ src/app/detalle/components/treemap/treemap.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Subject, merge } from 'rxjs';
+import { Subject } from 'rxjs';
 import { takeUntil, tap } from 'rxjs/operators';
 
 import * as Highcharts from 'highcharts';
@@ -25,22 +25,31 @@ export class TreemapComponent implements OnInit {
     // private _dataTreeMap: IIngresos | IGastos;
     private _dataTreeMap: IDataTreemap;
     private _tabSelected: number;
+    private _subTabSelectd: number;
     private _subTabSelectd1: string;
     private _subTabSelectd2: string;
     private _subTabSelectd4: string;
     private _codField: string;
     private _desField: string;
+    private _fields = { codigo: '', descripcion: '' };
     private _unsubscribe$ = new Subject<void>();
-    private tabMapping = {
-        11: { code: 'CodCap', description: 'DesCap' },
-        12: { code: 'CodArt', description: 'DesArt' },
-        13: { code: 'CodCon', description: 'DesCon' },
-        14: { code: 'CodEco', description: 'DesEco' },
-        1: { code: 'CodEco', description: 'DesEco' },
-        2: { code: 'CodPro', description: 'DesPro' },
-        3: { code: 'CodOrg', description: 'DesOrg' },
-        4: { code: 'CodEco', description: 'DesEco' },
-    };
+
+    // En prepareDataGastos.service.ts de asinan los diferentes nives a los campos CodPro o CodEco
+    // private tabMapping = {
+    //     11: { code: 'CodCap', description: 'DesCap' },
+    //     12: { code: 'CodArt', description: 'DesArt' },
+    //     13: { code: 'CodCon', description: 'DesCon' },
+    //     14: { code: 'CodEco', description: 'DesEco' },
+    //     21: { code: 'CodPro', description: 'DesPro' },
+    //     22: { code: 'CodPro', description: 'DesPro' },
+    //     23: { code: 'CodPro', description: 'DesPro' },
+    //     24: { code: 'CodPro', description: 'DesPro' },
+    //     31: { code: 'CodOrg', description: 'DesOrg' },
+    //     41: { code: 'CodEco', description: 'DesEco' },
+    //     42: { code: 'CodEco', description: 'DesEco' },
+    //     43: { code: 'CodEco', description: 'DesEco' },
+    //     44: { code: 'CodEco', description: 'DesEco' },
+    // };
 
     constructor(
         private _changeSubTabService: ChangeSubTabService,
@@ -53,19 +62,26 @@ export class TreemapComponent implements OnInit {
     ) {}
 
     ngOnInit() {
-        merge(
-            this._selectedTabNewService.source$.pipe(
+        this._selectedTabNewService.source$
+            .pipe(
                 tap((data) => {
                     this._tabSelected = data;
+                    console.log('Has cambiado de tab: ', data);
                     this.changeSubTabByTabSelected();
                 })
-            ),
-            this._changeSubTabService.source$.pipe(
+            )
+            .pipe(takeUntil(this._unsubscribe$))
+            .subscribe();
+
+        this._changeSubTabService.source$
+            .pipe(
                 tap((data) => {
+                    console.log('Has cambiado de subtab: ', data);
+                    this._codField = data.codField;
+                    this._desField = data.desField;
                     this.loadData(data.codField, data.desField);
                 })
             )
-        )
             .pipe(takeUntil(this._unsubscribe$))
             .subscribe();
     }
@@ -97,30 +113,88 @@ export class TreemapComponent implements OnInit {
             case 1:
                 switch (this._subTabSelectd1) {
                     case 'ingresosEconomicaCapitulos':
-                        this._tabSelected = 11;
+                        this._fields = { codigo: 'CodCap', descripcion: 'DesCap' };
+                        // this._subTabSelectd = 11;
                         break;
                     case 'ingresosEconomicaArticulos':
-                        this._tabSelected = 12;
+                        this._fields = { codigo: 'CodArt', descripcion: 'DesArt' };
+                        // this._subTabSelectd = 12;
                         break;
                     case 'ingresosEconomicaConceptos':
-                        this._tabSelected = 13;
+                        this._fields = { codigo: 'CodCon', descripcion: 'Descon' };
+                        // this._subTabSelectd = 13;
                         break;
                     case 'ingresosEconomicaEconomicos':
-                        this._tabSelected = 14;
+                        this._fields = { codigo: 'CodEco', descripcion: 'DesEco' };
+                        // this._subTabSelectd = 14;
                         break;
                     default:
                         break;
                 }
                 break;
 
+            case 2:
+                this._fields = { codigo: 'CodPro', descripcion: 'DesPro' };
+                // switch (this._subTabSelectd2) {
+                //     case 'Por áreas' || 'Por programas':
+                //         // this._subTabSelectd = 21;
+                //         this._fields = { codigo: 'CodPro', descripcion: 'DesPro' };
+                //         break;
+                //     case 'Por política':
+                //         // this._subTabSelectd = 22;
+                //         this._fields = { codigo: 'CodPro', descripcion: 'DesPro' };
+                //         break;
+                //     case 'Por grupo programas':
+                //         // this._subTabSelectd = 23;
+                //         this._fields = { codigo: 'CodPro', descripcion: 'DesPro' };
+                //         break;
+                //     // case 'Por programa':
+                //     //     // this._subTabSelectd = 24;
+                //     //     break;
+                //     default:
+                //         break;
+                // }
+                break;
+
+            case 3:
+                this._fields = { codigo: 'CodOrg', descripcion: 'DesOrg' };
+                // this._subTabSelectd = 31;
+                break;
+
+            case 4:
+                this._fields = { codigo: 'CodEco', descripcion: 'DesEco' };
+                // switch (this._subTabSelectd4) {
+                //     case 'Por capítulo gasto':
+                //         this._subTabSelectd = 41;
+                //         break;
+                //     case 'Por artículo':
+                //         this._subTabSelectd = 42;
+                //         break;
+                //     case 'Por concepto':
+                //         this._subTabSelectd = 43;
+                //         break;
+                //     case 'Por económico':
+                //         this._subTabSelectd = 44;
+                //         break;
+                //     default:
+                //         break;
+                // }
+                break;
             default:
                 break;
         }
 
-        const tabInfo = this.tabMapping[this._tabSelected];
-        if (tabInfo) {
-            this._changeSubTabService.changeSubTab(tabInfo.code, tabInfo.description);
-        }
+        this._changeSubTabService.changeSubTab(this._fields.codigo, this._fields.descripcion);
+        console.log('this._fields: ', this._fields);
+        // this.loadData(this._fields.codigo, this._fields.descripcion);
+
+        // const tabInfo = this.tabMapping[this._subTabSelectd];
+        // if (tabInfo) {
+        //     this._changeSubTabService.changeSubTab(tabInfo.code, tabInfo.description);
+        //     console.log('tabInfo: ', tabInfo);
+
+        //     // this.loadData(tabInfo.code, tabInfo.description);
+        // }
     }
 
     loadData(codField: string, desField: string) {
@@ -128,10 +202,12 @@ export class TreemapComponent implements OnInit {
 
         const loadData = this._dataStoreService.dataTable;
         this._dataTreeMap = this.dataTreemap(
-            this._tabSelected > 10 ? loadData.rowDataIngresos : loadData.rowDataGastos,
+            this._tabSelected === 1 ? loadData.rowDataIngresos : loadData.rowDataGastos,
             codField,
             desField
         );
+        console.log('this._dataTreeMap: ', this._dataTreeMap);
+
         this.showTreemap();
     }
 
