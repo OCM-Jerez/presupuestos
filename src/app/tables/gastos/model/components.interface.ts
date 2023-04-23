383a60c8f2ef35ff578c6fe89699705a31cb3b65
mamjerez@gmail.com <mamjerez@gmail.com>
2023-04-23 20:20:38 +0200

    Add key

--- src/app/tables/gastos/model/components.interface.ts
+++ src/app/tables/gastos/model/components.interface.ts
@@ -3,7 +3,13 @@ import { CLASIFICATION_TYPE } from '../../../commons/util/util';
 export interface IButtonClasification {
     clasificationType: CLASIFICATION_TYPE;
     name: string;
+    key: string;
     selected: boolean;
     codigo?: string;
     descripcion?: string;
+}
+export interface IButtonAdicional {
+    name: string;
+    path: string;
+    param?: string;
 }
