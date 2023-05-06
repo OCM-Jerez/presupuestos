4363e244e84b33d87c5dac602464bfd7a1506072
mamjerez@gmail.com <mamjerez@gmail.com>
2023-05-06 14:01:45 +0200

    Delete detalle.module.ts

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,21 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-
-import { environment } from './environments/environment';
-import { AppComponent } from './app/app.component';
-import { DetalleModule } from './app/detalle/detalle.module';
-import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { AppRoutingModule } from './app/app-routing.module';
 import { AvalaibleYearsService } from '@services/avalaibleYears.service';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(AppRoutingModule, BrowserModule, FormsModule, ReactiveFormsModule, DetalleModule),
+        importProvidersFrom(AppRoutingModule, BrowserModule, FormsModule, ReactiveFormsModule),
         AvalaibleYearsService,
         provideHttpClient(withInterceptorsFromDi()),
-    ]
-})
-  .catch(err => console.error(err));
+    ],
+}).catch((err) => console.error(err));
