5c259bd9e2b14784f5f4b788775af92d4ad1e8c7
mamjerez@gmail.com <mamjerez@gmail.com>
2023-05-06 13:26:07 +0200

    Bootstrap the project using standalone APIs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertService } from '@services/alert.service';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
+    standalone: true,
+    imports: [
+        NavbarComponent,
+        NgIf,
+        RouterOutlet,
+        FooterComponent,
+    ],
 })
 export class AppComponent implements OnInit {
     message = '';
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { DetalleModule } from './app/detalle/detalle.module';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AvalaibleYearsService } from '@services/avalaibleYears.service';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(AppRoutingModule, BrowserModule, FormsModule, ReactiveFormsModule, DetalleModule),
+        AvalaibleYearsService,
+        provideHttpClient(withInterceptorsFromDi()),
+    ]
+})
   .catch(err => console.error(err));
