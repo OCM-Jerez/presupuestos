fb4a5597fa8acec4c6b60db1b2671fd3a2876804
mamjerez@gmail.com <mamjerez@gmail.com>
2023-05-07 09:07:33 +0200

    selector: 'app-detalle'

--- src/app/detalle/detalle.component.ts
+++ src/app/detalle/detalle.component.ts
@@ -1,31 +1,31 @@
 import { Component } from '@angular/core';
-import { TableComponent } from './components/table/table.component';
+import { CheckboxComponent } from '../commons/components/checkbox/checkbox.component';
 import { SubtabsComponent } from './components/subtabs/subtabs.component';
+import { TablePresupuestoComponent } from './components/table-presupuesto/table-presupuesto.component';
+import { TableComponent } from './components/table/table.component';
 import { TabComponent } from './components/tabs/tab/tab.component';
 import { TabsComponent } from './components/tabs/tabs.component';
 import { TreemapComponent } from './components/treemap/treemap.component';
-import { TablePresupuestoComponent } from './components/table-presupuesto/table-presupuesto.component';
-import { CheckboxComponent } from '../commons/components/checkbox/checkbox.component';
 
 @Component({
-    selector: 'app-detalle-presupuesto',
-    templateUrl: './detalle.component.html',
-    styleUrls: ['./detalle.component.scss'],
-    standalone: true,
-    imports: [
-        CheckboxComponent,
-        TablePresupuestoComponent,
-        TreemapComponent,
-        TabsComponent,
-        TabComponent,
-        SubtabsComponent,
-        TableComponent,
-    ],
+	selector: 'app-detalle',
+	templateUrl: './detalle.component.html',
+	styleUrls: ['./detalle.component.scss'],
+	standalone: true,
+	imports: [
+		CheckboxComponent,
+		TablePresupuestoComponent,
+		TreemapComponent,
+		TabsComponent,
+		TabComponent,
+		SubtabsComponent,
+		TableComponent
+	]
 })
 export class DetalleComponent {
-    constructor() {}
+	constructor() {}
 
-    async hasChangeCheckbox() {
-        // await this._tableService.loadData(this._typeClasification);
-    }
+	async hasChangeCheckbox() {
+		// await this._tableService.loadData(this._typeClasification);
+	}
 }
