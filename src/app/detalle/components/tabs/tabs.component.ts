5bc9afa6725abc29c03dff4dc9b7fb1f1f879f89
mamjerez@gmail.com <mamjerez@gmail.com>
2023-05-07 13:45:44 +0200

    1er step with signal

--- src/app/detalle/components/tabs/tabs.component.ts
+++ src/app/detalle/components/tabs/tabs.component.ts
@@ -1,51 +1,54 @@
-import { AfterContentInit, Component, ContentChildren, OnInit, QueryList } from '@angular/core';
+import { NgClass, NgFor } from '@angular/common';
+import { AfterContentInit, Component, ContentChildren, QueryList, signal } from '@angular/core';
 
 import { TabComponent } from './tab/tab.component';
 
 import { SelectedTabService } from '@services/selectedTab.service';
 
 import { CLASIFICATION_TYPE } from '@appTypes/clasification.type';
-import { NgFor, NgClass } from '@angular/common';
 
 @Component({
-    selector: 'app-tabs',
-    templateUrl: './tabs.component.html',
-    styleUrls: ['./tabs.component.scss'],
-    standalone: true,
-    imports: [NgFor, NgClass],
+	selector: 'app-tabs',
+	templateUrl: './tabs.component.html',
+	styleUrls: ['./tabs.component.scss'],
+	standalone: true,
+	imports: [NgFor, NgClass]
 })
-export class TabsComponent implements OnInit, AfterContentInit {
-    // El operador de aserción de no nulo ! se utiliza para asegurar
-    // que la variable tabs no será nula en tiempo de ejecución.
-    @ContentChildren(TabComponent) tabs!: QueryList<TabComponent>;
+export class TabsComponent implements AfterContentInit {
+	// El operador de aserción de no nulo ! se utiliza para asegurar
+	// que la variable tabs no será nula en tiempo de ejecución.
+	@ContentChildren(TabComponent) tabs!: QueryList<TabComponent>;
 
-    constructor(private _selectedTabService: SelectedTabService) {}
-    async ngOnInit(): Promise<void> {}
+	public tabSignal = signal<CLASIFICATION_TYPE>('ingresosEconomicaEconomicos');
 
-    ngAfterContentInit(): void {
-        const activeTabs = this.tabs.filter((tab) => tab.active);
-        if (activeTabs.length === 0) {
-            this.tabs.first.active = true;
-        }
-    }
+	constructor(private _selectedTabService: SelectedTabService) {}
 
-    async clickTab(tab: TabComponent): Promise<void> {
-        const tabs = this.tabs.toArray();
-        const tabActive = tabs.find((tab) => tab.active);
+	ngAfterContentInit(): void {
+		const activeTabs = this.tabs.filter((tab) => tab.active);
+		if (activeTabs.length === 0) {
+			this.tabs.first.active = true;
+		}
+	}
 
-        if (tabActive && tabActive.title !== tab.title) {
-            this.tabs.toArray().forEach((tab) => (tab.active = false));
-            tab.active = true;
-        }
+	async clickTab(tab: TabComponent): Promise<void> {
+		const tabs = this.tabs.toArray();
+		const tabActive = tabs.find((tab) => tab.active);
 
-        const tabDataMap = {
-            Ingresos: 'ingresosEconomicaEconomicos',
-            '¿En qué se gasta?': 'gastosProgramaProgramas',
-            '¿Quién lo gasta?': 'gastosOrganicaOrganicos',
-            '¿Para qué se gasta?': 'gastosEconomicaEconomicos',
-        };
+		if (tabActive && tabActive.title !== tab.title) {
+			this.tabs.toArray().forEach((tab) => (tab.active = false));
+			tab.active = true;
+		}
 
-        const clasificationType: CLASIFICATION_TYPE = tabDataMap[tab.title];
-        this._selectedTabService.setSelectedTab(clasificationType);
-    }
+		const tabDataMap = {
+			Ingresos: 'ingresosEconomicaEconomicos',
+			'¿En qué se gasta?': 'gastosProgramaProgramas',
+			'¿Quién lo gasta?': 'gastosOrganicaOrganicos',
+			'¿Para qué se gasta?': 'gastosEconomicaEconomicos'
+		};
+
+		this.tabSignal.set(tabDataMap[tab.title]);
+
+		const clasificationType: CLASIFICATION_TYPE = tabDataMap[tab.title];
+		this._selectedTabService.setSelectedTab(clasificationType);
+	}
 }
