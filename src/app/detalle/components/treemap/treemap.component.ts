4502bd3ed93a66fb703eb1a7b2a7ce7142c9cc40
mamjerez@gmail.com <mamjerez@gmail.com>
2023-05-08 11:10:55 +0200

    Show treemap when change subtab FUNCIONA

--- src/app/detalle/components/treemap/treemap.component.ts
+++ src/app/detalle/components/treemap/treemap.component.ts
@@ -11,12 +11,13 @@ import { SelectedSubTab2Service } from '@services/selectedSubTab2.service';
 import { SelectedSubTab4Service } from '@services/selectedSubTab4.service';
 import { SelectedTabService } from '@services/selectedTab.service';
 
-import { IDataTreemap } from '@interfaces/dataTreemap.interface';
-
 import { IDataTable } from '@interfaces/dataTable.interface';
+import { IDataTreemap } from '@interfaces/dataTreemap.interface';
 import { TableService } from '@services/table.service';
+
 import * as Highcharts from 'highcharts';
 import HighchartsTreemap from 'highcharts/modules/treemap';
+import { CLASIFICATION_TYPE } from '../../../commons/types/clasification.type';
 HighchartsTreemap(Highcharts);
 @Component({
   selector: 'app-treemap',
@@ -55,6 +56,8 @@ export class TreemapComponent implements OnInit {
   subscribeToServices(): void {
     this._selectedSubTab1Service.source$.subscribe((data) => {
       this._subTabSelectd1 = data;
+      // console.log('this._subTabSelectd1', this._subTabSelectd1);
+      this.setFields();
     });
 
     this._selectedSubTab2Service.source$.subscribe((data) => {
@@ -96,16 +99,16 @@ export class TreemapComponent implements OnInit {
     switch (this._tabSelected) {
       case 'ingresosEconomicaEconomicos':
         switch (this._subTabSelectd1) {
-          case 'ingresosEconomicaCapitulos':
+          case 'Por capítulo ingresos':
             this._fields = { codigo: 'CodCap', descripcion: 'DesCap' };
             break;
-          case 'ingresosEconomicaArticulos':
+          case 'Por artículo':
             this._fields = { codigo: 'CodArt', descripcion: 'DesArt' };
             break;
-          case 'ingresosEconomicaConceptos':
+          case 'Por concepto':
             this._fields = { codigo: 'CodCon', descripcion: 'DesCon' };
             break;
-          case 'ingresosEconomicaEconomicos':
+          case 'Por económico':
             this._fields = { codigo: 'CodEco', descripcion: 'DesEco' };
             break;
           default:
@@ -152,11 +155,23 @@ export class TreemapComponent implements OnInit {
   }
 
   async calcSeries(codField: string, desField: string) {
-    const data =
-      this._tabSelected === 'ingresosEconomicaEconomicos'
-        ? this._dataStoreService.dataTable.rowDataIngresos
-        : this._dataStoreService.dataTable.rowDataGastos;
-    this._dataTreeMap = this._prepareDataTreemapService.calcSeries(data, codField, desField, 'Definitivas2023');
+    // console.log(this._tabSelected);
+    // console.log(this._subTabSelectd1);
+    if (this._tabSelected === undefined) {
+      this._tabSelected = 'ingresosEconomicaEconomicos';
+    }
+    // console.log(this._tabSelected);
+    const data = await this._tableService.loadData(this._tabSelected as CLASIFICATION_TYPE);
+    // console.log('data', data.rowDataIngresos);
+
+    const dataGraph = this._tabSelected === 'ingresosEconomicaEconomicos' ? data.rowDataIngresos : data.rowDataGastos;
+    // console.log('dataGraph', dataGraph);
+    // console.log('codField', codField);
+    // console.log('desField', desField);
+
+    this._dataTreeMap = this._prepareDataTreemapService.calcSeries(dataGraph, codField, desField, 'Definitivas2023');
+    // console.log('this._dataTreeMap', this._dataTreeMap);
+
     this.showTreemap();
   }
 
