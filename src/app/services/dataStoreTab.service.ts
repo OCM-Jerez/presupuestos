c4868fb622ac71b51a6611ab712c7ee90c25519d
mamjerez@gmail.com <mamjerez@gmail.com>
2023-05-18 18:42:10 +0200

    Create DataStoreTabService

--- src/app/services/dataStoreTab.service.ts
+++ src/app/services/dataStoreTab.service.ts
@@ -1,17 +1,47 @@
+// import { Injectable } from '@angular/core';
+// import { ITab } from '@interfaces/tab.interface';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class DataStoreTabService {
+//   private data: ITab;
+
+//   setData(data: ITab): void {
+//     this.data = data;
+//   }
+
+//   getData(): ITab {
+//     return this.data;
+//   }
+// }
+
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
 import { ITab } from '@interfaces/tab.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStoreTabService {
-  private data: ITab;
+  private tabSubject: BehaviorSubject<ITab>;
 
-  setData(data: ITab): void {
-    this.data = data;
+  constructor() {
+    // Initialize with a default tab
+    this.tabSubject = new BehaviorSubject<ITab>({
+      clasificationType: 'ingresosEconomicaEconomicos',
+      name: '',
+      key: '',
+      selected: false
+    });
   }
 
-  getData(): ITab {
-    return this.data;
+  getTab(): Observable<ITab> {
+    return this.tabSubject.asObservable();
+  }
+
+  setTab(tab: ITab): void {
+    this.tabSubject.next(tab);
   }
 }
