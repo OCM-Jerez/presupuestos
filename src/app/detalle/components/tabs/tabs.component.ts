d2504638416c9d72d908e5d614055109eed6d70a
mamjerez@gmail.com <mamjerez@gmail.com>
2023-05-18 18:42:39 +0200

    Optimizo code. FUNCIONA

--- src/app/detalle/components/tabs/tabs.component.ts
+++ src/app/detalle/components/tabs/tabs.component.ts
@@ -1,9 +1,12 @@
 import { NgClass, NgFor } from '@angular/common';
-import { AfterContentInit, Component, ContentChildren, QueryList } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, OnDestroy, QueryList } from '@angular/core';
 
 import { TabComponent } from './tab/tab.component';
 
-import { SelectedTabService } from '@services/selectedTab.service';
+import { Subject, takeUntil } from 'rxjs';
+
+import { ITab } from '@interfaces/tab.interface';
+import { DataStoreTabService } from '../../../services/dataStoreTab.service';
 
 @Component({
   selector: 'app-tabs',
@@ -12,40 +15,56 @@ import { SelectedTabService } from '@services/selectedTab.service';
   standalone: true,
   imports: [NgFor, NgClass]
 })
-export class TabsComponent implements AfterContentInit {
+export class TabsComponent implements AfterContentInit, OnDestroy {
   // El operador de aserción de no nulo ! se utiliza para asegurar
   // que la variable tabs no será nula en tiempo de ejecución.
   @ContentChildren(TabComponent) tabs!: QueryList<TabComponent>;
+  private _tabSelected: ITab;
+  private _unsubscribe$ = new Subject<void>();
+  private tabDataMap = {
+    Ingresos: 'ingresosEconomicaEconomicos',
+    '¿En qué se gasta?': 'gastosProgramaProgramas',
+    '¿Quién lo gasta?': 'gastosOrganicaOrganicos',
+    '¿Para qué se gasta?': 'gastosEconomicaEconomicos'
+  };
 
-  constructor(private _selectedTabService: SelectedTabService) {}
+  constructor(private _dataStoreTabService: DataStoreTabService) {
+    this._dataStoreTabService
+      .getTab()
+      .pipe(takeUntil(this._unsubscribe$))
+      .subscribe((storeTab) => {
+        this._tabSelected = storeTab;
+      });
+  }
 
   ngAfterContentInit(): void {
-    // Si no hay pestañas activas, activa la primera
-    if (!this.tabs.some((tab) => tab.active)) {
-      this.tabs.first.active = true;
-    }
+    this.setActiveTab();
+  }
+
+  ngOnDestroy() {
+    this._unsubscribe$.next();
+    this._unsubscribe$.complete();
   }
 
   async clickTab(tab: TabComponent): Promise<void> {
-    console.log(tab);
-    console.log(this.tabs);
-    console.log(this.tabs.toArray());
+    this.setActiveTab(tab);
 
-    const tabs = this.tabs.toArray();
-    const tabActive = tabs.find((tab) => tab.active);
+    this._dataStoreTabService.setTab({
+      clasificationType: this.tabDataMap[tab.title],
+      name: '',
+      key: '',
+      selected: false
+    });
+  }
 
-    if (tabActive && tabActive.title !== tab.title) {
-      tabs.forEach((tab) => (tab.active = false));
-      tab.active = true;
-    }
-
-    const tabDataMap = {
-      Ingresos: 'ingresosEconomicaEconomicos',
-      '¿En qué se gasta?': 'gastosProgramaProgramas',
-      '¿Quién lo gasta?': 'gastosOrganicaOrganicos',
-      '¿Para qué se gasta?': 'gastosEconomicaEconomicos'
-    };
-
-    this._selectedTabService.setSelectedTab(tabDataMap[tab.title]);
+  private setActiveTab(tab?: TabComponent): void {
+    const tabsArray = this.tabs.toArray();
+    tabsArray.forEach((t) => {
+      if (tab ? t.title === tab.title : this.tabDataMap[t.title] === this._tabSelected.clasificationType) {
+        t.active = true;
+      } else {
+        t.active = false;
+      }
+    });
   }
 }
