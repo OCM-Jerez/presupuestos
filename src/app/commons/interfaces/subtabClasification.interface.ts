12c64e79950a231474f2f17665701ee388517b1d
mamjerez@gmail.com <mamjerez@gmail.com>
2023-05-19 11:06:28 +0200

    Sort items

--- src/app/commons/interfaces/subtabClasification.interface.ts
+++ src/app/commons/interfaces/subtabClasification.interface.ts
@@ -2,9 +2,9 @@ import { CLASIFICATION_TYPE } from '@appTypes/clasification.type';
 
 export interface ISubtabClasification {
   clasificationType: CLASIFICATION_TYPE;
-  name: string;
-  key: string;
-  selected: boolean;
   codField?: string;
   desField?: string;
+  key: string;
+  name: string;
+  selected: boolean;
 }
