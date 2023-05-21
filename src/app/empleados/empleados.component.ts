5efb761730cf5e7fa5babdb1092326b3713e3587
mamjerez@gmail.com <mamjerez@gmail.com>
2023-05-21 13:35:14 +0200

    Use card to ../commons/com../card/card.component

--- src/app/empleados/empleados.component.ts
+++ src/app/empleados/empleados.component.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+
+import { CardIndiceComponent } from '../commons/components/card/card.component';
 
 @Component({
   selector: 'app-empleados',
   templateUrl: './empleados.component.html',
   styleUrls: ['./empleados.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [NgFor, CardIndiceComponent]
 })
-export class EmpleadosComponent {}
+export class EmpleadosComponent {
+  items = [
+    {
+      indice: '1.',
+      title: 'Funcionarios',
+      footer:
+        'En desarrollo...',
+      img: "../../assets/img/indice/busget-stream-1.webp"
+    },
+    {
+      indice: '2.',
+      title: 'Laborales',
+      footer:
+        'En desarrollo...',
+      img: "../../assets/img/indice/busget-stream-2.webp"
+    },
+    {
+      indice: '3.',
+      title: 'Eventuales',
+      footer:
+        'En desarrollo...',
+      img: "../../assets/img/indice/busget-stream-3.webp"
+    }
+  ];
+}
