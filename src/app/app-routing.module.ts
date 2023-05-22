be3aab66c9e30573ced4e748e69081c84a9bc772
mamjerez@gmail.com <mamjerez@gmail.com>
2023-05-22 16:59:50 +0200

    Delete children[ ]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,51 +2,51 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  // path: '',
+  // children: [
   {
-    path: '',
-    children: [
-      {
-        path: 'home',
-        loadComponent: () => import('./home/home.component')
-      },
-      {
-        path: 'visionGlobal',
-        loadComponent: () => import('./vision-global/vision-global.component')
-      },
-      {
-        path: 'detalle',
-        loadComponent: () => import('./detalle/detalle.component')
-      },
-      {
-        path: 'empleados',
-        loadComponent: () => import('./empleados/empleados.component')
-      },
-      {
-        path: 'explicamos',
-        loadComponent: () => import('./explicamos/explicamos.component')
-      },
-      {
-        path: 'glosario',
-        loadComponent: () => import('./glosario/glosario.component')
-      },
-      {
-        path: 'graphDetalle',
-        loadComponent: () =>
-          import('./graphs/graph-detalle/graph-detalle.component')
-      },
-      {
-        path: 'tableProgramaDetails',
-        loadComponent: () =>
-          import('./tables/table-programa-details/table-programa-details.component')
-      },
-      {
-        path: 'tableGrupoProgramaDetails/:origen',
-        loadComponent: () =>
-          import('./tables/table-gastos-gruposprogramas-details/table-gastos-gruposprogramas-details.component')
-      },
-      { path: '**', pathMatch: 'full', redirectTo: 'home' }
-    ]
-  }
+    path: 'home',
+    loadComponent: () => import('./home/home.component')
+  },
+  {
+    path: 'visionGlobal',
+    loadComponent: () => import('./vision-global/vision-global.component')
+  },
+  {
+    path: 'detalle',
+    loadComponent: () => import('./detalle/detalle.component')
+  },
+  {
+    path: 'empleados',
+    loadComponent: () => import('./empleados/empleados.component')
+  },
+  {
+    path: 'explicamos',
+    loadComponent: () => import('./explicamos/explicamos.component')
+  },
+  {
+    path: 'glosario',
+    loadComponent: () => import('./glosario/glosario.component')
+  },
+  {
+    path: 'graphDetalle',
+    loadComponent: () =>
+      import('./graphs/graph-detalle/graph-detalle.component')
+  },
+  {
+    path: 'tableProgramaDetails',
+    loadComponent: () =>
+      import('./tables/table-programa-details/table-programa-details.component')
+  },
+  {
+    path: 'tableGrupoProgramaDetails/:origen',
+    loadComponent: () =>
+      import('./tables/table-gastos-gruposprogramas-details/table-gastos-gruposprogramas-details.component')
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+  // ]
+  // }
 ];
 
 @NgModule({
