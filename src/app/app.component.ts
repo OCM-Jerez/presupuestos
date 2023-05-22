fe5333261890b36b0ec73b98743832671c54e53f
mamjerez@gmail.com <mamjerez@gmail.com>
2023-05-22 18:04:30 +0200

    Clean code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 import { FooterComponent } from './layouts/footer/footer.component';
@@ -11,6 +11,7 @@ import { NavbarComponent } from './layouts/navbar/navbar.component';
   standalone: true,
   imports: [NavbarComponent, NgIf, RouterOutlet, FooterComponent]
 })
+
 export class AppComponent implements OnInit {
   ngOnInit(): void {
     localStorage.removeItem('selected_years');
