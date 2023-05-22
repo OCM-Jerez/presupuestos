208eccdb04204d15202e26657c3d4c7a16f9aebd
mamjerez@gmail.com <mamjerez@gmail.com>
2023-05-22 18:02:12 +0200

    Clean code

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -7,6 +7,7 @@ import packageJson from '../../../../package.json';
   styleUrls: ['./footer.component.scss'],
   standalone: true
 })
+
 export class FooterComponent {
   version = packageJson.version;
 }
