11b28e5ca2e92e9eec496f822231fb8cdb2dea13
mamjerez@gmail.com <mamjerez@gmail.com>
2023-05-26 10:06:09 +0200

    Delete sankey gastos

--- src/app/vision-global/components/sankey-graphs/sankey-graphs.component.ts
+++ src/app/vision-global/components/sankey-graphs/sankey-graphs.component.ts
@@ -10,15 +10,16 @@ HighchartsMore(Highcharts);
 HighchartsSankey(Highcharts);
 
 @Component({
-	selector: 'app-sankey-graph-gastos',
+	selector: 'app-sankey-graphs',
 	standalone: true,
 	imports: [CommonModule],
-	templateUrl: './sankey-graph-gastos.component.html',
-	styleUrls: ['./sankey-graph-gastos.component.scss']
+	templateUrl: './sankey-graphs.component.html',
+	styleUrls: ['./sankey-graphs.component.scss']
 })
-export class SankeyGraphGastosComponent implements OnInit {
+export class SankeyGraphsComponent implements OnInit {
 	private _dataStoreService = inject(DataStoreService);
 
+	private _dataIngresos: any;
 	private _dataGastos: any;
 
 	ngOnInit(): void {
@@ -26,8 +27,38 @@ export class SankeyGraphGastosComponent implements OnInit {
 	}
 
 	private async _loadData() {
+		await this.calcSumIngresos();
+		this._showGraphIngresos();
 		await this.calcSumGastos();
-		this._showGraph();
+		this._showGraphGastos();
+	}
+
+	async calcSumIngresos() {
+		this._dataIngresos = this._dataStoreService.dataTable.rowDataIngresos;
+
+		// Creo array de Capitulos de ingresos.
+		let capitulos = [];
+		for (const item of this._dataIngresos) {
+			const value = {
+				name: `${item.CodCap}-${item.DesCap}`,
+				value: item.Definitivas2023,
+				recaudado: item.DerechosReconocidosNetos2023
+			};
+			capitulos.push(value);
+		}
+
+		// Totalizo por capitulo de ingreso
+		this._dataIngresos = capitulos.reduce((acc, curr) => {
+			const index = acc.findIndex((item) => item.name === curr.name);
+			index > -1
+				? ((acc[index].value += curr.value), (acc[index].recaudado += curr.recaudado))
+				: acc.push({
+						name: curr.name,
+						value: curr.value,
+						recaudado: curr.recaudado
+				  });
+			return acc;
+		}, []);
 	}
 
 	async calcSumGastos() {
@@ -58,7 +89,65 @@ export class SankeyGraphGastosComponent implements OnInit {
 		}, []);
 	}
 
-	_showGraph() {
+	_showGraphIngresos() {
+		Highcharts.chart('ingresos', {
+			accessibility: {
+				enabled: false
+			},
+			title: {
+				text: "<span style= 'font-size: 25px'>Ingresos</span>"
+			},
+			subtitle: {
+				text: ''
+			},
+			tooltip: {
+				headerFormat: null,
+				pointFormat: '{point.fromNode.name} \u2192 {point.toNode.name}: {point.weight} ',
+				nodeFormat: '{point.name}: {point.sum} '
+			},
+			series: [
+				{
+					borderColor: '#1a1a1a',
+					borderWidth: 1,
+					colors: [
+						'#1E90FF',
+						'#1eff8d',
+						'#ce9eff',
+						'#1eff8d',
+						'#1eff8d',
+						'#1eff8d',
+						'#1eff8d',
+						'#ce9eff',
+						'#ce9eff',
+						'#ce9eff',
+						'#ce9eff'
+					],
+					keys: ['from', 'to', 'weight'],
+					data: [
+						[this._dataIngresos[0].name, 'Presupuesto', this._dataIngresos[0].value],
+						[this._dataIngresos[1].name, 'Presupuesto', this._dataIngresos[1].value],
+						[this._dataIngresos[2].name, 'Presupuesto', this._dataIngresos[2].value],
+						[this._dataIngresos[3].name, 'Presupuesto', this._dataIngresos[3].value],
+						[this._dataIngresos[4].name, 'Presupuesto', this._dataIngresos[4].value],
+						[this._dataIngresos[5].name, 'Presupuesto', this._dataIngresos[5].value],
+						[this._dataIngresos[6].name, 'Presupuesto', this._dataIngresos[6].value],
+						[this._dataIngresos[7].name, 'Presupuesto', this._dataIngresos[7].value],
+						[this._dataIngresos[8].name, 'Presupuesto', this._dataIngresos[8].value]
+					],
+					type: 'sankey',
+					name: 'Ingresos',
+					dataLabels: {
+						style: {
+							color: '#1a1a1a',
+							textOutline: false
+						}
+					}
+				}
+			]
+		} as any);
+	}
+
+	_showGraphGastos() {
 		Highcharts.chart('gastos', {
 			accessibility: {
 				enabled: false
