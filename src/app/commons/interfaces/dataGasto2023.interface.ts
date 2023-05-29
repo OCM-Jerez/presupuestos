94e36cadd46859a0d1745dc79941a8d7d03b8949
mamjerez@gmail.com <mamjerez@gmail.com>
2023-05-29 20:33:55 +0200

    Add Interrfaces

---
+++ src/app/commons/interfaces/dataGasto2023.interface.ts
@@ -0,0 +1,28 @@
+export interface IDataGasto2023 {
+	CodPro: string;
+	DesPro: string;
+	CodOrg: string;
+	DesOrg: string;
+	CodCap: string;
+	DesCap: string;
+	CodEco: string;
+	DesEco: string;
+	Iniciales2023: number;
+	Modificaciones2023: number;
+	Definitivas2023: number;
+	GastosComprometidos2023: number;
+	ObligacionesReconocidasNetas2023: number;
+	Pagos2023: number;
+	ObligacionesPendientePago2023: number;
+	RemanenteCredito2023: number;
+	CodAre?: number;
+	DesAre?: string;
+	CodPol?: number;
+	DesPol?: string;
+	CodGru?: number;
+	DesGru?: string;
+	CodCon?: number;
+	DesCon?: string;
+	CodArt?: number;
+	DesArt?: string;
+}
