02cc764c8b05f54cbb02a225f136165a2d9c17ea
mamjerez@gmail.com <mamjerez@gmail.com>
2023-05-29 04:45:56 +0200

    Fix warning  'inject' is defined but never used

--- src/app/empleados/components/rpt/rpt.component.ts
+++ src/app/empleados/components/rpt/rpt.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, inject } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // import '@ag-grid-enterprise';
 
