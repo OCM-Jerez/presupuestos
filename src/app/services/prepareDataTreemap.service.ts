a8624be766e0a8e9db65c053e2a902134a5ab26f
mamjerez@gmail.com <mamjerez@gmail.com>
2023-05-30 17:42:09 +0200

    Use import { colors } from ...

--- src/app/services/prepareDataTreemap.service.ts
+++ src/app/services/prepareDataTreemap.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+
+import { colors } from '@app/commons/consts/colorsTreemap';
+
 import { IDataGasto2023 } from '@interfaces/dataGasto2023.interface';
 import { IDataIngreso2023 } from '@interfaces/dataIngreso2023.interface';
 
@@ -7,8 +10,7 @@ import { IDataIngreso2023 } from '@interfaces/dataIngreso2023.interface';
 })
 export class PrepareDataTreemapService {
 	calcSeries(data: IDataGasto2023[] & IDataIngreso2023[], codigo, descripcion, campoSumatorio, aRestar?) {
-		let array = [];
-		array = data.reduce((acc, curr) => {
+		const array = data.reduce((acc, curr) => {
 			const item =
 				aRestar === undefined
 					? {
@@ -19,8 +21,7 @@ export class PrepareDataTreemapService {
 							name: curr[codigo] + '-' + curr[descripcion],
 							value: curr[campoSumatorio] - curr[aRestar]
 					  };
-			acc.push(item);
-			return acc;
+			return [...acc, item];
 		}, []);
 
 		// Totalizo
@@ -38,39 +39,8 @@ export class PrepareDataTreemapService {
 		dataTotal = dataTotal.sort((a, b) => b.value - a.value);
 		dataTotal = dataTotal.slice(0, 25);
 
-		const colors = [
-			'#2f7ed8',
-			'#097E17',
-			'#8bbc21',
-			'#910000',
-			'#1aadce',
-			'#492970',
-			'#f28f43',
-			'#77a1e5',
-			'#c42525',
-			'#a6c96a',
-			'#7cb5ec',
-			'#003DF6',
-			'#90ed7d',
-			'#f7a35c',
-			'#8085e9',
-			'#f15c80',
-			'#e4d354',
-			'#2b908f',
-			'#f45b5b',
-			'#91e8e1',
-			'#f15c80',
-			'#e4d354',
-			'#2b908f',
-			'#f45b5b',
-			'#91e8e1'
-		];
-
-		let colorIndex = -1;
-
-		dataTotal.map((item) => {
-			colorIndex += 1;
-			colorIndex > 25 ? (colorIndex = 0) : colorIndex;
+		dataTotal.map((item, index) => {
+			const colorIndex = index % 25;
 			item.color = colors[colorIndex];
 			item.euros = item.value.toLocaleString('de-DE') + ' €';
 			item.name = item.name + '  ' + item.euros;
