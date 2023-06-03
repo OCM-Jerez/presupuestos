e819d5382b733e3d387c3c4527bc20950f315716
mamjerez@gmail.com <mamjerez@gmail.com>
2023-06-03 21:36:50 +0200

    Working in issue  #62

--- src/app/detalle/detalle.component.ts
+++ src/app/detalle/detalle.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 
 import { CheckboxComponent } from '../commons/components/checkbox/checkbox.component';
 import { SubtabsComponent } from './components/subtabs/subtabs.component';
@@ -8,8 +8,11 @@ import { TabComponent } from './components/tabs/tab/tab.component';
 import { TabsComponent } from './components/tabs/tabs.component';
 import { TreemapComponent } from './components/treemap/treemap.component';
 
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { ITab } from '@interfaces/tab.interface';
+import { AvalaibleYearsService } from '@services/avalaibleYears.service';
+import { takeUntil, tap } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
 	selector: 'app-detalle',
@@ -24,16 +27,29 @@ import { ITab } from '@interfaces/tab.interface';
 		TablePresupuestoComponent,
 		TabsComponent,
 		TreemapComponent,
-		NgFor
+		NgFor,
+		NgIf
 	]
 })
-export default class DetalleComponent {
+export default class DetalleComponent implements OnInit {
+	private _avalaibleYearsService = inject(AvalaibleYearsService);
+	// private _lengthYears = 0;
+	public multiYears = true;
+	private subscription: Subscription;
+
 	public tabs: ITab[] = [
 		{ clasificationType: 'ingresosEconomicaEconomicos', title: 'Ingresos', selected: true },
 		{ clasificationType: 'gastosProgramaProgramas', title: '¿En qué se gasta?', selected: false },
 		{ clasificationType: 'gastosOrganicaOrganicos', title: '¿Quién lo gasta?', selected: false },
 		{ clasificationType: 'gastosEconomicaEconomicos', title: '¿Para qué se gasta?', selected: false }
 	];
+
+	ngOnInit(): void {
+		this.subscription = this._avalaibleYearsService.yearsSubject$.subscribe((year) => {
+			// this._lengthYears = year.length;
+			year.length === 1 ? (this.multiYears = true) : (this.multiYears = false);
+		});
+	}
 
 	async hasChangeCheckbox() {
 		// await this._tableService.loadData(this._typeClasification);
