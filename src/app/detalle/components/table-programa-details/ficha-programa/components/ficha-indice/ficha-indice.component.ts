7fcb4e26109c830ada48879df51bcfd4b31d720f
mamjerez@gmail.com <mamjerez@gmail.com>
2023-06-21 07:37:13 +0200

    Add FichaIndiceComponent

---
+++ src/app/detalle/components/table-programa-details/ficha-programa/components/ficha-indice/ficha-indice.component.ts
@@ -0,0 +1,115 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { CardMenuComponent } from '@app/commons/components/card-menu/card-menu.component';
+
+@Component({
+	selector: 'app-ficha-indice',
+	standalone: true,
+	imports: [CommonModule, CardMenuComponent, NgFor],
+	templateUrl: './ficha-indice.component.html',
+	styleUrls: ['./ficha-indice.component.scss']
+})
+export class FichaIndiceComponent {
+	private _router = inject(Router);
+
+	cardMenus = [
+		{
+			rutaImagen: 'assets/img/home/menu1-400x250.webp',
+			titulo: 'Presupuesto',
+			subtitulo: ' ',
+			funcion: () => this.visionGlobal(),
+			textButton: 'Visión global',
+			background: 'linear-gradient(to bottom, #FFFDFC , #FCE1CB)'
+		},
+		{
+			rutaImagen: 'assets/img/home/menu4-400x250.webp',
+			titulo: 'Empleados municipales',
+			subtitulo: '',
+			funcion: () => this.empleados(),
+			textButton: 'Empleados',
+			background: 'linear-gradient(to bottom, #F5F5F5 , #E0E0E0)'
+		},
+		{
+			rutaImagen: 'assets/img/home/menu2-400x250.webp',
+			titulo: 'Detalle del presupuesto',
+			subtitulo: '',
+			funcion: () => this.detalle(),
+			textButton: 'Detalle',
+			background: 'linear-gradient(to bottom, #FCFEFF , #CDE9FE)'
+		},
+		{
+			rutaImagen: 'assets/img/home/menu3-400x250.webp',
+			titulo: 'Licitaciones',
+			subtitulo: '',
+			funcion: () => this.licitaciones(),
+			textButton: 'Licitaciones',
+			background: 'linear-gradient(to bottom, #F1F8E9 , #DCEDC8)'
+		},
+		{
+			rutaImagen: 'assets/img/home/menu1-400x250.webp',
+			titulo: 'Presupuesto',
+			subtitulo: ' ',
+			funcion: () => this.visionGlobal(),
+			textButton: 'Visión global',
+			background: 'linear-gradient(to bottom, #FFFDFC , #FCE1CB)'
+		},
+		{
+			rutaImagen: 'assets/img/home/menu4-400x250.webp',
+			titulo: 'Empleados municipales',
+			subtitulo: '',
+			funcion: () => this.empleados(),
+			textButton: 'Empleados',
+			background: 'linear-gradient(to bottom, #F5F5F5 , #E0E0E0)'
+		},
+		{
+			rutaImagen: 'assets/img/home/menu2-400x250.webp',
+			titulo: 'Detalle del presupuesto',
+			subtitulo: '',
+			funcion: () => this.detalle(),
+			textButton: 'Detalle',
+			background: 'linear-gradient(to bottom, #FCFEFF , #CDE9FE)'
+		},
+		{
+			rutaImagen: 'assets/img/home/menu3-400x250.webp',
+			titulo: 'Licitaciones',
+			subtitulo: '',
+			funcion: () => this.licitaciones(),
+			textButton: 'Licitaciones',
+			background: 'linear-gradient(to bottom, #F1F8E9 , #DCEDC8)'
+		},
+		{
+			rutaImagen: 'assets/img/home/menu4-400x250.webp',
+			titulo: 'Empleados municipales',
+			subtitulo: '',
+			funcion: () => this.empleados(),
+			textButton: 'Empleados',
+			background: 'linear-gradient(to bottom, #F5F5F5 , #E0E0E0)'
+		},
+		{
+			rutaImagen: 'assets/img/home/menu2-400x250.webp',
+			titulo: 'Detalle del presupuesto',
+			subtitulo: '',
+			funcion: () => this.detalle(),
+			textButton: 'Detalle',
+			background: 'linear-gradient(to bottom, #FCFEFF , #CDE9FE)'
+		}
+	];
+
+	visionGlobal() {
+		this._router.navigateByUrl('/visionGlobal');
+	}
+
+	detalle() {
+		this._router.navigateByUrl('/detalle');
+	}
+
+	licitaciones() {
+		window.open('https://con.ocmjerez.org/', '_blank');
+	}
+
+	empleados() {
+		this._router.navigateByUrl('/empleados');
+	}
+}
--- src/app/detalle/components/table-programa-details/ficha-programa/ficha-programa.component.ts
+++ src/app/detalle/components/table-programa-details/ficha-programa/ficha-programa.component.ts
@@ -5,16 +5,26 @@ import { TabComponent } from '@app/commons/components/tabs/tab/tab.component';
 import { TabsComponent } from '@app/commons/components/tabs/tabs.component';
 import { FichaPresupuestoComponent } from './components/ficha-presupuesto/ficha-presupuesto.component';
 import { FichaPersonalComponent } from './components/ficha-personal/ficha-personal.component';
+import { FichaIndiceComponent } from './components/ficha-indice/ficha-indice.component';
 
 @Component({
 	selector: 'app-ficha-programa',
 	standalone: true,
-	imports: [TabComponent, TabsComponent, NgFor, NgIf, FichaPresupuestoComponent, FichaPersonalComponent],
+	imports: [
+		TabComponent,
+		TabsComponent,
+		NgFor,
+		NgIf,
+		FichaIndiceComponent,
+		FichaPresupuestoComponent,
+		FichaPersonalComponent
+	],
 	templateUrl: './ficha-programa.component.html',
 	styleUrls: ['./ficha-programa.component.scss']
 })
 export default class FichaProgramaComponent {
 	public tabs: { title: string; selected: boolean; isFicha: boolean }[] = [
+		{ title: 'Indice', selected: true, isFicha: true },
 		{ title: 'Presupuesto', selected: true, isFicha: true },
 		{ title: 'Personal', selected: false, isFicha: true },
 		{ title: 'Carta de servicios', selected: false, isFicha: true },
