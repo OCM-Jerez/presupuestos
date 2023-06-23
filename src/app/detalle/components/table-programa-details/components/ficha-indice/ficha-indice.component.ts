b4169b202106c84180f919fc3dd93ec4e64833e8
mamjerez@gmail.com <mamjerez@gmail.com>
2023-06-23 09:09:55 +0200

    textButton: this.totalPresupuestado.toLocaleString

--- src/app/detalle/components/table-programa-details/components/ficha-indice/ficha-indice.component.ts
+++ src/app/detalle/components/table-programa-details/components/ficha-indice/ficha-indice.component.ts
@@ -21,97 +21,9 @@ export default class FichaIndiceComponent implements OnInit, OnDestroy {
 	private _subscription: Subscription;
 	private _datos: IGastos[] = [];
 	public programa: string;
-
-	cardsInfo = [
-		{
-			rutaImagen: 'assets/img/home/menu1-400x250.webp',
-			titulo: 'Presupuesto',
-			subtitulo: 'Breve explicación del dato correspondiente ... ',
-			funcion: () => this.fichaPresupuesto(),
-			textButton: '18.854.736',
-			background: 'linear-gradient(to bottom,  #FCE1CB, white)'
-		},
-		{
-			rutaImagen: 'assets/img/home/menu1-400x250.webp',
-			titulo: 'Gastos a 5/06/2023',
-			subtitulo: 'Breve explicación del dato correspondiente ... ',
-			funcion: () => this.fichaPresupuesto(),
-			textButton: '8.468.783',
-			background: 'linear-gradient(to bottom, #EEBE3E, white)'
-		},
-		{
-			rutaImagen: 'assets/img/home/menu4-400x250.webp',
-			titulo: 'Empleados',
-			subtitulo: '',
-			funcion: () => this.fichaEmpleados(),
-			textButton: '326',
-			background: 'linear-gradient(to bottom, #E0E0E0, white)'
-		},
-		{
-			rutaImagen: 'assets/img/home/menu2-400x250.webp',
-			titulo: 'Carta de servicios',
-			subtitulo: '',
-			funcion: () => this.fichaEmpleados(),
-			textButton: 'No',
-			background: 'linear-gradient(to bottom, #CDE9FE, white)'
-		},
-		{
-			rutaImagen: 'assets/img/home/menu3-400x250.webp',
-			titulo: 'Indicadores',
-			subtitulo: '',
-			funcion: () => this.fichaEmpleados(),
-			textButton: 'No',
-			background: 'linear-gradient(to bottom,#DCEDC8, white)'
-		},
-		{
-			rutaImagen: 'assets/img/home/menu1-400x250.webp',
-			titulo: 'Hemeroteca',
-			subtitulo: ' ',
-			funcion: () => this.fichaPresupuesto(),
-			textButton: ' 12 entradas',
-			background: 'linear-gradient(to bottom, #FCE1CB, white)'
-		},
-		{
-			rutaImagen: 'assets/img/home/menu4-400x250.webp',
-			titulo: 'Documentos',
-			subtitulo: '',
-			funcion: () => this.fichaEmpleados(),
-			textButton: 'No',
-			background: 'linear-gradient(to bottom, #D3CCE3 , white)'
-		},
-		{
-			rutaImagen: 'assets/img/home/menu2-400x250.webp',
-			titulo: 'Licitaciones',
-			subtitulo: '',
-			funcion: () => this.fichaEmpleados(),
-			textButton: '12',
-			background: 'linear-gradient(to bottom,#5092A9 , #FCFDFE)'
-		},
-		{
-			rutaImagen: 'assets/img/home/menu3-400x250.webp',
-			titulo: 'Contratos menores',
-			subtitulo: '',
-			funcion: () => this.fichaEmpleados(),
-			textButton: '14',
-			background: 'linear-gradient(to bottom, #EEBE3E ,white)'
-		},
-		{
-			rutaImagen: 'assets/img/home/menu4-400x250.webp',
-			titulo: 'Acuerdos de Pleno',
-			subtitulo: '',
-			funcion: () => this.fichaEmpleados(),
-			textButton: '2',
-			background: 'linear-gradient(to bottom, #D2DAB9 , white)'
-		}
-		// {
-		// 	rutaImagen: 'assets/img/home/menu2-400x250.webp',
-		// 	titulo: 'Acuerdos JGL',
-		// 	subtitulo: '',
-		// 	funcion: () => this.fichaEmpleados(),
-		// 	textButton: '8',
-		// 	background: 'linear-gradient(to bottom, #A28B7B , white)'
-		// }
-	];
+	private totalPresupuestado: number;
+	private totalGastado: number;
+	public cardsInfo: any[] = [];
 
 	fichaPresupuesto() {
 		this._router.navigateByUrl('/fichaPresupuesto');
@@ -125,6 +37,106 @@ export default class FichaIndiceComponent implements OnInit, OnDestroy {
 			this._datos = data;
 		});
 		this.programa = this._datos[0].CodPro + ' - ' + this._datos[0].DesPro;
+		this.totalPresupuestado = this._datos.reduce((acc, item) => {
+			acc += item.Definitivas2023;
+			return acc;
+		}, 0);
+
+		this.totalGastado = this._datos.reduce((acc, item) => {
+			acc += item.Pagos2023;
+			return acc;
+		}, 0);
+
+		this.cardsInfo = [
+			{
+				rutaImagen: 'assets/img/home/menu1-400x250.webp',
+				titulo: 'Presupuesto',
+				subtitulo: 'Breve explicación del dato correspondiente ... ',
+				funcion: () => this.fichaPresupuesto(),
+				textButton: this.totalPresupuestado.toLocaleString('de-DE'),
+				background: 'linear-gradient(to bottom,  #FCE1CB, white)'
+			},
+			{
+				rutaImagen: 'assets/img/home/menu1-400x250.webp',
+				titulo: 'Gastos a 5/06/2023',
+				subtitulo: 'Breve explicación del dato correspondiente ... ',
+				funcion: () => this.fichaPresupuesto(),
+				textButton: this.totalGastado.toLocaleString('de-DE'),
+				background: 'linear-gradient(to bottom, #EEBE3E, white)'
+			},
+			{
+				rutaImagen: 'assets/img/home/menu4-400x250.webp',
+				titulo: 'Empleados',
+				subtitulo: '',
+				funcion: () => this.fichaEmpleados(),
+				textButton: '326',
+				background: 'linear-gradient(to bottom, #E0E0E0, white)'
+			},
+			{
+				rutaImagen: 'assets/img/home/menu2-400x250.webp',
+				titulo: 'Carta de servicios',
+				subtitulo: '',
+				funcion: () => this.fichaEmpleados(),
+				textButton: 'No',
+				background: 'linear-gradient(to bottom, #CDE9FE, white)'
+			},
+			{
+				rutaImagen: 'assets/img/home/menu3-400x250.webp',
+				titulo: 'Indicadores',
+				subtitulo: '',
+				funcion: () => this.fichaEmpleados(),
+				textButton: 'No',
+				background: 'linear-gradient(to bottom,#DCEDC8, white)'
+			},
+			{
+				rutaImagen: 'assets/img/home/menu1-400x250.webp',
+				titulo: 'Hemeroteca',
+				subtitulo: ' ',
+				funcion: () => this.fichaPresupuesto(),
+				textButton: ' 12 entradas',
+				background: 'linear-gradient(to bottom, #FCE1CB, white)'
+			},
+			{
+				rutaImagen: 'assets/img/home/menu4-400x250.webp',
+				titulo: 'Documentos',
+				subtitulo: '',
+				funcion: () => this.fichaEmpleados(),
+				textButton: 'No',
+				background: 'linear-gradient(to bottom, #D3CCE3 , white)'
+			},
+			{
+				rutaImagen: 'assets/img/home/menu2-400x250.webp',
+				titulo: 'Licitaciones',
+				subtitulo: '',
+				funcion: () => this.fichaEmpleados(),
+				textButton: '12',
+				background: 'linear-gradient(to bottom,#5092A9 , #FCFDFE)'
+			},
+			{
+				rutaImagen: 'assets/img/home/menu3-400x250.webp',
+				titulo: 'Contratos menores',
+				subtitulo: '',
+				funcion: () => this.fichaEmpleados(),
+				textButton: '14',
+				background: 'linear-gradient(to bottom, #EEBE3E ,white)'
+			},
+			{
+				rutaImagen: 'assets/img/home/menu4-400x250.webp',
+				titulo: 'Acuerdos de Pleno',
+				subtitulo: '',
+				funcion: () => this.fichaEmpleados(),
+				textButton: '2',
+				background: 'linear-gradient(to bottom, #D2DAB9 , white)'
+			}
+			// {
+			// 	rutaImagen: 'assets/img/home/menu2-400x250.webp',
+			// 	titulo: 'Acuerdos JGL',
+			// 	subtitulo: '',
+			// 	funcion: () => this.fichaEmpleados(),
+			// 	textButton: '8',
+			// 	background: 'linear-gradient(to bottom, #A28B7B , white)'
+			// }
+		];
 	}
 
 	ngOnDestroy() {
