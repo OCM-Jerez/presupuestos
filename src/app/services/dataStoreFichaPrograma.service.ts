a77143f20d1349ed443008ea495ae010532d1371
mamjerez@gmail.com <mamjerez@gmail.com>
2023-07-22 10:35:11 +0200

    Clean code

--- src/app/services/dataStoreFichaPrograma.service.ts
+++ src/app/services/dataStoreFichaPrograma.service.ts
@@ -1,47 +1,13 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
-// import { IGastos } from '@interfaces/gastos.interface';
 import { IDataGasto } from '@interfaces/dataGasto.interface';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class DataStoreFichaProgramaService {
-	// private fichaProgramaSubject: BehaviorSubject<IGastos[]>;
 	private fichaProgramaSubject: BehaviorSubject<IDataGasto[]>;
-
-	// constructor() {
-	// 	this.fichaProgramaSubject = new BehaviorSubject<IGastos[]>([
-	// 		{
-	// 			CodCap: 0,
-	// 			CodEco: 0,
-	// 			CodOrg: 0,
-	// 			CodPro: 0,
-	// 			Definitivas2023: 0,
-	// 			DesCap: '',
-	// 			DesEco: '',
-	// 			DesOrg: '',
-	// 			DesPro: '',
-	// 			GastosComprometidos2023: 0,
-	// 			Iniciales2023: 0,
-	// 			Modificaciones2023: 0,
-	// 			ObligacionesPendientePago2023: 0,
-	// 			ObligacionesReconocidasNetas2023: 0,
-	// 			Pagos2023: 0,
-	// 			RemanenteCredito2023: 0,
-	// 			appPresupuestaria: ''
-	// 		}
-	// 	]);
-	// }
-
-	// getFichaProgramaData(): Observable<IGastos[]> {
-	// 	return this.fichaProgramaSubject.asObservable();
-	// }
-
-	// setFichaProgramaData(fichaPrograma: IGastos[]): void {
-	// 	this.fichaProgramaSubject.next(fichaPrograma);
-	// }
 
 	constructor() {
 		this.fichaProgramaSubject = new BehaviorSubject<IDataGasto[]>([
