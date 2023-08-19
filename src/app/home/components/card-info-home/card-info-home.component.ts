e3b1612009af568afdfb3d1c807ec37950aabf0e
mamjerez@gmail.com <mamjerez@gmail.com>
2023-08-19 08:19:25 +0200

    Change card-info to card-info-home

---
+++ src/app/home/components/card-info-home/card-info-home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-card-info-home',
+	templateUrl: './card-info-home.component.html',
+	styleUrls: ['./card-info-home.component.scss'],
+	standalone: true
+})
+export class CardInfoHomeComponent {}
