cef0f20f43b8be03dad67e92911964f159585098
mamjerez@gmail.com <mamjerez@gmail.com>
2023-11-02 10:38:43 +0100

    Update info

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   currentYear: 2023,
-  liqDate2022: '01/12/2022',
+  liqDate2022: '31/12/2022',
   liqDate2023: '08/08/2023'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   currentYear: 2023,
-  liqDate2022: '01/12/2022',
+  liqDate2022: '31/12/2022',
   liqDate2023: '08/08/2023'
 };
 
