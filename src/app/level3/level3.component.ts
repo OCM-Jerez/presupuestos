5a2bd23f9f8ce2d0b6c588cb1ad1e8d63780ac63
mamjerez@gmail.com <mamjerez@gmail.com>
2023-11-26 20:01:51 +0100

    Clean code

--- src/app/level3/level3.component.ts
+++ src/app/level3/level3.component.ts
@@ -80,66 +80,30 @@ export default class Level3Component implements OnInit {
 			case 'mesas':
 				path = `art10/infoInstitucional`;
 				break;
-
 			case 'retribuciones2022':
 				console.log('retribuciones2022');
 				path = `art10/infoInstitucional/retribuciones2022`;
 				break;
-
 			case 'rpt':
 				console.log('retribuciones2022');
 				path = `art10/infoInstitucional/retribuciones2022`;
 				break;
-
 			case 'empleadosNews':
-				// console.log('empleadosNews');
-				// src\assets\empleados\empleadosNews.json
 				path = `empleadosNews`;
 				break;
-			// case 'subvencion':
-			// this.isSubvencion = true;
-			// break;
-			// case 'tema':
-			// this._isTema = true;
-			// break;
-			// case 'edificioSingular':
-			// 	console.log('edificioSingular');
-			// 	path = `mediomabiental/edificiosSingulares/:edificio`;
-			// this.isEdificioSingular = true;
-			// this.imgURL = `/assets/${parametro}/${this._option}/${this._option}.jpg`;
-			// break;
-			// case 'licitacion':
-			// this.isLicitacion = true;
-			// this.imgURL = `/assets/${parametro}/${this._option}/${this._option}.jpg`;
-			// break;
-			// case 'distrito':
-			// this.isDistrito = true;
-			// this.imgURL = `/assets/${parametro}/${this._option}/${this._option}.jpg`;
-			// break;
 		}
 
 		const pathBase = `/assets/${path}/`;
 		// console.log('pathBase', pathBase);
 
 		const commonRequests = {
-			// data: this._http.get<IOption[]>(`${pathBase}${this._option}/${this._option}.json`),
 			coms: this._http.get<ICom[]>(`${pathBase}${this.path}/${this.path}Coms.json`),
 			docs: this._http.get<IDoc[]>(`${pathBase}${this.path}/${this.path}Docs.json`),
 			news: this._http.get<INew[]>(`${pathBase}${this.path}/${this.path}News.json`)
 		};
 
-		// const stepsSubvencionRequest = this.isSubvencion
-		// 	? { steps: this._http.get<IStepSubvencion[]>(`${pathBase}${this._option}/${this._option}Steps.json`) }
-		// 	: {};
-
-		// const stepsRequest = this.isLicitacion
-		// 	? { steps: this._http.get<IStep[]>(`${pathBase}${this._option}/${this._option}Steps.json`) }
-		// 	: {};
-
 		const allRequests = {
 			...commonRequests
-			// ...stepsRequest,
-			// ...stepsSubvencionRequest
 		};
 		// console.log('allRequests', allRequests);
 
@@ -148,32 +112,16 @@ export default class Level3Component implements OnInit {
 				catchError((error) => {
 					console.error('Error fetching data:', error);
 					return of({
-						// data: [],
 						coms: [],
 						docs: [],
 						news: []
-						// steps: this.isLicitacion ? [] : undefined
 					});
 				})
 			)
 			.subscribe((response) => {
-				// this.data = response.data;
 				this.docs = response.docs;
 				this.coms = response.coms;
 				this.news = response.news;
-
-				// if (this.isSubvencion || this.isLicitacion) {
-				// 	this.stepsSubvencion = response.steps as IStepSubvencion[];
-				// }
-
-				// if (this.isLicitacion) {
-				// 	this.steps = response.steps as IStep[];
-				// }
-
-				// const descripcionObj = this.data.find((obj) => obj.data === 'Descripción');
-				// if (descripcionObj) {
-				// 	this.descripcion = descripcionObj.value;
-				// }
 
 				this.hasComs = this.coms.length > 1;
 				this.hasDocs = this.docs.length > 1;
