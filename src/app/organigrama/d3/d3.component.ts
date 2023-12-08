e6968fa535ee2f4cee5835cb57ece772dc5b3598
mamjerez@gmail.com <mamjerez@gmail.com>
2023-12-08 07:36:21 +0100

    Delete pagingStep

--- src/app/organigrama/d3/d3.component.ts
+++ src/app/organigrama/d3/d3.component.ts
@@ -219,23 +219,6 @@ export default class D3Component implements AfterViewInit {
 						break;
 				}
 			})
-			// .pagingStep((d) => 5)
-			// .minPagingVisibleNodes((d) => 14)
-			// .pagingButton((d, i, arr, state) => {
-			// const step = state.pagingStep(d.parent);
-			// const currentIndex = d.parent.data._pagingStep;
-			// const diff = d.parent.data._directSubordinatesPaging - currentIndex;
-			// 	const min = Math.min(diff, step);
-			// 	return `
-			// 			   <div style="margin-top:50px;">
-			// 				  <div style="display:flex;width:170px;border-radius:20px;padding:5px 15px; padding-bottom:4px;;background-color:#E5E9F2">
-			// 				  <div><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-			// 				  <path d="M5.59 7.41L10.18 12L5.59 16.59L7 18L13 12L7 6L5.59 7.41ZM16 6H18V18H16V6Z" fill="#716E7B" stroke="#716E7B"/>
-			// 				  </svg>
-			// 				  </div><div style="line-height:2"> Show next ${min}  nodes </div></div>
-			// 			   </div>
-			// 			`;
-			// })
 			.nodeWidth((d) => 160 + 2)
 			.nodeHeight((d) => 150 + 25)
 			.childrenMargin((d) => 50)
@@ -271,7 +254,6 @@ export default class D3Component implements AfterViewInit {
 				</div>
 			  </div>
 			`;
-				// <div style="display:flex;justify-content:flex-end;margin-top:5px;margin-right:8px">#${d.data.id}</div>
 			})
 			.render();
 	}
