67812dfbbca25cfac42b3ed60f8d58f55d835b3f
mamjerez@gmail.com <mamjerez@gmail.com>
2023-12-14 09:54:23 +0100

    Add licitacion plazaBelen2023

--- src/app/art15/components/licitaciones/licitaciones.component.ts
+++ src/app/art15/components/licitaciones/licitaciones.component.ts
@@ -45,7 +45,8 @@ export default class LicitacionesComponent {
 		this.createCard('Suministro de equipamiento informático (hardware y software).', 'equiposInformaticos2023'),
 		this.createCard('Suministro de software BIM (Building Information Modeling) ', 'softwareBIM2023'),
 		this.createCard('Suministro de un autobús eléctrico 7,50 metros', 'microbusElectrico2023'),
-		this.createCard('Suministro de equipos de topografía', 'equiposTopografia2023')
+		this.createCard('Suministro de equipos de topografía', 'equiposTopografia2023'),
+		this.createCard('Espacios de sombra en Plaza Belén', 'plazaBelen2023')
 	];
 
 	licitacionesESP = [];
