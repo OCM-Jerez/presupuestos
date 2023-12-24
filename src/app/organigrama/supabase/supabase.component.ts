b4aa0d21ce90e084b09fb3826feac4e37aeac11f
mamjerez@gmail.com <mamjerez@gmail.com>
2023-12-24 20:02:50 +0100

    Borro todo lo referente a graphQL

--- src/app/organigrama/supabase/supabase.component.ts
+++ src/app/organigrama/supabase/supabase.component.ts
@@ -1,132 +1,41 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { SupabaseService } from './supabase.service';
 
 @Component({
 	selector: 'app-supabase',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [NgIf],
 	templateUrl: './supabase.component.html',
 	styleUrls: ['./supabase.component.scss']
 })
 export default class SupabaseComponent implements OnInit {
-	s;
 	@Input() id?: number;
+	//TODO: - Add types
 	public positionData: any[] = null;
-	// public employeeData: any[] = null;
-	// public positionExternalData: any[] = null;
-	// public positionExternalData11: any[] = null;
-	// public recordData: any[] = null;
-
 	public employeeName: string = null;
 	public hasEmployeeLinkedin = false;
 	public hasEmployeeWikipedia = false;
 	public hasPositionExternal = false;
 	public hasPositionExternal11 = true;
-
 	public hasRecord = false;
 	public hasRecordLinkedin = false;
 
-	constructor(private supabaseService: SupabaseService) {}
+	private _supabaseService = inject(SupabaseService);
 
 	ngOnInit(): void {
-		// this.fetchData();
 		this.fetchDataFromView();
-		// console.log(this.id);
-		// this.useGraphQL();
 	}
-
-	// async fetchData() {
-	// 	try {
-	// 		this.positionData = await this.supabaseService.fetchTableData('positions', this.id);
-	// 	} catch (error) {
-	// 		console.error('Error fetching data:', error);
-	// 	}
-
-	// 	try {
-	// 		this.employeeData = await this.supabaseService.fetchTableData('employees', this.id);
-	// 		if (this.employeeData[0].linkedin_url) this.hasEmployeeLinkedin = true;
-	// 		if (this.employeeData[0].wikipedia_url) this.hasEmployeeWikipedia = true;
-	// 		this.employeeName = `${this.employeeData[0].name} ${this.employeeData[0].firstname} ${this.employeeData[0].lastname}`;
-	// 	} catch (error) {
-	// 		console.error('Error fetching data:', error);
-	// 	}
-
-	// 	try {
-	// 		this.positionExternalData = await this.supabaseService.fetchTableData('positions_external', this.id);
-	// 		// console.log(this.positionExternalData);
-
-	// 		if (this.positionExternalData[0].position !== null) this.hasPositionExternal = true;
-	// 	} catch (error) {
-	// 		console.error('Error fetching data:', error);
-	// 	}
-
-	// 	try {
-	// 		this.positionExternalData11 = await this.supabaseService.fetchTableData('positions_external', 11);
-	// 		// console.log(this.positionExternalData11);
-
-	// 		if (this.positionExternalData11[0].position !== null) this.hasPositionExternal11 = true;
-	// 	} catch (error) {
-	// 		console.error('Error fetching data:', error);
-	// 	}
-
-	// 	try {
-	// 		this.recordData = await this.supabaseService.fetchTableData('records', this.id);
-	// 		if (this.recordData[0].name !== null) this.hasRecord = true;
-	// 		if (this.recordData[0].linkedin_url) this.hasRecordLinkedin = true;
-	// 	} catch (error) {
-	// 		console.error('Error fetching data:', error);
-	// 	}
-	// }
 
 	async fetchDataFromView() {
 		try {
-			this.positionData = await this.supabaseService.fetchTableDataFromView(this.id);
-			console.log(this.id);
-
+			this.positionData = await this._supabaseService.fetchTableDataFromView(this.id);
+			// TODO:  Nombre completo en el server
 			this.employeeName = `${this.positionData[0].name} ${this.positionData[0].firstname} ${this.positionData[0].lastname}`;
 			this.hasPositionExternal = this.positionData[0].position_ext;
 			this.hasRecord = this.positionData[0].record_name;
 		} catch (error) {
 			console.error('Error fetching data:', error);
 		}
-	}
-
-	async useGraphQL() {
-		const data = await this.supabaseService.fetchQuery('employees', this.id);
-		console.log(data);
-
-		const jsonData = {
-			data: {
-				employeesCollection: {
-					edges: [
-						{
-							node: {
-								id: 1,
-								name: 'María José',
-								salary: 'Ayuntamiento = 74.135,35 Senado= 28.658,14 Total= 102.793,49  ',
-								ayto_url:
-									'https://transparencia.jerez.es/infopublica/institucional/corporacion/2023-2027/maria-jose-garcia-pelayo',
-								lastname: 'Pelayo',
-								firstname: 'García'
-							}
-						}
-					]
-				},
-				positionsCollection: {
-					edges: [
-						{
-							node: {
-								id: 1,
-								position: 'Alcaldía-Presidencia'
-							}
-						}
-					]
-				}
-			}
-		};
-
-		const idValue = jsonData.data.employeesCollection.edges[0].node.name;
-		console.log(idValue); // Esto mostrará 101
 	}
 }
--- src/app/organigrama/supabase/supabase.service.ts
+++ src/app/organigrama/supabase/supabase.service.ts
@@ -6,128 +6,25 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js';
 	providedIn: 'root'
 })
 export class SupabaseService {
-	private supabase: SupabaseClient;
+	private _supabase: SupabaseClient;
 
 	constructor() {
 		const supabaseUrl = 'https://cswdadlxiubwdzvqzywc.supabase.co';
 		const supabaseKey =
 			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzd2RhZGx4aXVid2R6dnF6eXdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI2MTkxODAsImV4cCI6MjAxODE5NTE4MH0.m2uvFmJvUoNc2qLndRZT0WhpMARVk7D4L1QveHsoapQ';
-		this.supabase = createClient(supabaseUrl, supabaseKey);
+		this._supabase = createClient(supabaseUrl, supabaseKey);
 	}
 
 	async fetchTableData(tableName: string, id: number): Promise<any> {
-		const { data, error } = await this.supabase.from(tableName).select('*').eq('id', id);
+		const { data, error } = await this._supabase.from(tableName).select('*').eq('id', id);
 		if (error) throw error;
 		return data;
 	}
 
 	async fetchTableDataFromView(id: number): Promise<any> {
-		const { data, error } = await this.supabase.from('employee_details').select('*').eq('id', id);
+		const { data, error } = await this._supabase.from('employee_details').select('*').eq('id', id);
 		if (error) throw error;
 		console.log(JSON.stringify(data));
 		return data;
-	}
-
-	async fetchTableDataJSON(tableName: string, id: number): Promise<any> {
-		const { data, error } = await this.supabase.from(tableName).select('*').eq('id', id);
-		if (error) throw error;
-		console.log(JSON.stringify(data));
-
-		return new Response(JSON.stringify({ data }), {
-			headers: { 'Content-Type': 'application/json' },
-			status: 200
-		});
-	}
-
-	async fetchQuery(tableName: string, id: number): Promise<any> {
-		const sql = `select
-		p.id as position_id,
-		p.employee_id as position_employee_id,
-		p.position as position_position,
-		p.type as position_type,
-		p.url as position_url,
-		p.obs as position_obs,
-		e.id as employee_id,
-		e.name as employee_name,
-		e.firstname as employee_firstname,
-		e.lastname as employee_lastname,
-		e.image as employee_image,
-		e.salary as employee_salary,
-		e.ayto_url as employee_ayto_url,
-		e.cv as employee_cv,
-		e.assets as employee_assets,
-		e.contact as employee_contact,
-		e.linkedin_url as employee_linkedin_url,
-		e.wikipedia_url as employee_wikipedia_url,
-		pe.position as position_external_position,
-		pe.salary as position_external_salary,
-		pe.url as position_external_url,
-		pe.obs as position_external_obs,
-		r.id as record_id,
-		r.date as record_date,
-		r.name as record_name,
-		r.firstname as record_firsname,
-		r.lastname as record_lastname,
-		r.salary as record_salary,
-		r.obs as record_obse,
-		r.wikipedia_url as record_wikipedia_url,
-		r.linkedin_url as record_linkedin_url,
-		r.image as record_image
-	  from
-		employees e
-		join positions p on e.id = p.employee_id
-		JOIN positions_external pe ON e.id = pe.employee_id
-		join records r on p.id = r.id
-	  where
-		e.id = 1;
-	  `;
-
-		const { data, error } = await this.supabase.from('employees').select(`select
-		p.id as position_id,
-		p.employee_id as position_employee_id,
-		p.position as position_position,
-		p.type as position_type,
-		p.url as position_url,
-		p.obs as position_obs,
-		e.id as employee_id,
-		e.name as employee_name,
-		e.firstname as employee_firstname,
-		e.lastname as employee_lastname,
-		e.image as employee_image,
-		e.salary as employee_salary,
-		e.ayto_url as employee_ayto_url,
-		e.cv as employee_cv,
-		e.assets as employee_assets,
-		e.contact as employee_contact,
-		e.linkedin_url as employee_linkedin_url,
-		e.wikipedia_url as employee_wikipedia_url,
-		pe.position as position_external_position,
-		pe.salary as position_external_salary,
-		pe.url as position_external_url,
-		pe.obs as position_external_obs,
-		r.id as record_id,
-		r.date as record_date,
-		r.name as record_name,
-		r.firstname as record_firsname,
-		r.lastname as record_lastname,
-		r.salary as record_salary,
-		r.obs as record_obse,
-		r.wikipedia_url as record_wikipedia_url,
-		r.linkedin_url as record_linkedin_url,
-		r.image as record_image
-	  from
-		employees e
-		join positions p on e.id = p.employee_id
-		JOIN positions_external pe ON e.id = pe.employee_id
-		join records r on p.id = r.id
-	  where
-		e.id = 1;
-	  `);
-
-		if (error) {
-			console.error('Error al ejecutar la consulta SQL:', error.message);
-		} else {
-			console.log('Resultado de la consulta SQL:', data);
-		}
 	}
 }
