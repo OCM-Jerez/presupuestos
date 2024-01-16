9a7a215b10630933afc84034ea97d0cf53065f4d
mamjerez@gmail.com <mamjerez@gmail.com>
2024-01-16 11:56:30 +0100

    Use tag

--- src/app/commons/components/level/noticias/noticias.component.ts
+++ src/app/commons/components/level/noticias/noticias.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, Input, OnInit, ViewChild, ViewContainerRef, inject } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import NewsFormComponent from '@app/addNewRecodsToSupabase/news-form/news-form.component';
 import { ModalService } from '@app/layouts/modal/modal.service';
@@ -13,12 +13,10 @@ import { TagStoreService } from '@services/tagStore.service';
 	templateUrl: './noticias.component.html',
 	styleUrls: ['./noticias.component.scss']
 })
-export default class NoticiasComponent implements OnInit {
-	// @ViewChild('view', { static: true, read: ViewContainerRef })
+export default class NoticiasComponent {
 	@ViewChild('modal')
 	modal!: ElementRef<HTMLDivElement>;
 	@ViewChild('overlay') overlay!: ElementRef<HTMLDivElement>;
-	// vcr!: ViewContainerRef;
 	@Input() news: any[];
 	private _router = inject(Router);
 	private _route = inject(ActivatedRoute);
@@ -27,27 +25,9 @@ export default class NoticiasComponent implements OnInit {
 	private _param = '';
 	public canAddRowSupabase = environment.canAddRowSupabase;
 
-	ngOnInit(): void {
-		const routeConfig = this._route.snapshot.routeConfig;
-		const paramMap = this._route.snapshot.paramMap;
-
-		if (routeConfig && routeConfig.path) {
-			const pathSegments = routeConfig.path.split('/');
-			const dynamicSegment = pathSegments.find((segment) => segment.startsWith(':'));
-			if (dynamicSegment) {
-				// Extraer el nombre del parámetro dinámico (sin los dos puntos ':')
-				const paramName = dynamicSegment.substring(1);
-				this._param = paramMap.get(paramName);
-			} else {
-				this._param = routeConfig.path.includes('/') ? routeConfig.path.split('/')[1] : routeConfig.path;
-			}
-		} else {
-			console.log('Configuración de ruta no disponible');
-		}
-	}
-
 	addNew(): void {
-		this._tagStoreService.setTag(this._param);
+		const paramMap = this._route.snapshot.paramMap;
+		this._tagStoreService.setTag(paramMap.get('tag'));
 		this._router.navigateByUrl(`/addNew/${this._param}`);
 		// this.useModal();
 	}
