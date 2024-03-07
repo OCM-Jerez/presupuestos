8f66f409fd27fe79d696785ad053a76e9931e5e2
mamjerez@gmail.com <mamjerez@gmail.com>
2024-03-07 16:13:32 +0100

    Refactor Level1Component imports and ngOnInit() method

--- src/app/level1/level1.component.ts
+++ src/app/level1/level1.component.ts
@@ -8,10 +8,8 @@ import NoticiasComponent from '@app/commons/components/level/noticias/noticias.c
 import ComentariosComponent from '@app/commons/components/level/comentarios/comentarios.component';
 import DocumentosComponent from '@app/commons/components/level/documentos/documentos.component';
 
+import { SupabaseService } from '@services/supabase.service';
 import { EnsureTitleService } from '@services/ensureTitle.service';
-import { TagStoreService } from '@services/tagStore.service';
-import { TitleStoreService } from '@services/titleStore.service';
-import { PathStoreService } from '@services/pathStore.service';
 import { GetNewsComsDocs } from '@services/getNewsComsDocs.service';
 
 import { IMenuItem } from '@interfaces/menu.interface';
@@ -27,51 +25,44 @@ import { INew } from '@interfaces/new.interface';
 })
 export default class Level1Component implements OnInit {
 	@Input() tag: string;
+	private _supabaseService = inject(SupabaseService);
 	private _ensureTitleService = inject(EnsureTitleService);
-	private _pathStoreService = inject(PathStoreService);
-	private _tagStoreService = inject(TagStoreService);
-	private _titleStoreService = inject(TitleStoreService);
 	private _router = inject(Router);
 	private _getNewsComsDocs = inject(GetNewsComsDocs);
 	public menuOptions: IMenuItem[] = [];
 	public coms: ICom[] = [];
 	public docs: IDoc[] = [];
 	public news: INew[] = [];
-	public title = this._titleStoreService.getTitle();
+	public title: string;
 
-	ngOnInit() {
-		// const tag = this._tagStoreService.getTag();
-		import(`../../assets/menuOptions/level1/${this.tag}.json`).then(async (data) => {
-			this.menuOptions = data.default.map((item: IMenuItem) => {
-				const modifiedItem = {
-					...item,
-					rutaImagen: environment.pathImgSupabase + item.tag + '.jpg'
-				};
+	async ngOnInit() {
+		const data = await this._supabaseService.fetchDataByLevel('level1', this.tag);
+		this.menuOptions = data.map((item: IMenuItem) => {
+			const modifiedItem = {
+				...item,
+				rutaImagen: environment.pathImgSupabase + item.tag + '.jpg'
+			};
 
-				return this.createCardMenu(modifiedItem);
-			});
-
-			await this.ensureTitle();
-
-			[this.news, this.coms, this.docs] = await this._getNewsComsDocs.fetchDataFromSupabase(this.tag);
+			return this.createCardMenu(modifiedItem);
 		});
+
+		await this.ensureTitle();
+
+		[this.news, this.coms, this.docs] = await this._getNewsComsDocs.fetchDataFromSupabase(this.tag);
 	}
 
 	createCardMenu(item: IMenuItem) {
 		let URL = item.isLastLevel ? 'levelLast/' + item.tag : 'level2/' + item.tag;
 
-		if (item.path === 'licitaciones') {
+		if (item.tag === 'licitaciones') {
 			URL = 'licitaciones';
 		}
-		if (item.path === 'presupuestos') {
+		if (item.tag === 'presupuestos') {
 			URL = 'presupuestos';
 		}
 		return {
 			...item,
 			funcion: () => {
-				this._pathStoreService.setPath(item.path);
-				this._tagStoreService.setTag(item.tag);
-				this._titleStoreService.setTitle(item.title);
 				this._router.navigateByUrl(URL);
 			}
 		};
