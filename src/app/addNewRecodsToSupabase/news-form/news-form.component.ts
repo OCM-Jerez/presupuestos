b9f632e764c2c9057ea9a597b74e8b5831302c93
mamjerez@gmail.com <mamjerez@gmail.com>
2024-03-08 10:13:59 +0100

    Add input and import statements to news-form.component.ts

--- src/app/addNewRecodsToSupabase/news-form/news-form.component.ts
+++ src/app/addNewRecodsToSupabase/news-form/news-form.component.ts
@@ -1,9 +1,11 @@
 import { Location } from '@angular/common';
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+// import { ActivatedRoute, Router } from '@angular/router';
 
 import { SupabaseService } from '@services/supabase.service';
-import { TagStoreService } from '@services/tagStore.service';
+// import { TagStoreService } from '@services/tagStore.service';
+// import { first } from 'rxjs';
 
 @Component({
 	selector: 'app-news-form',
@@ -13,14 +15,20 @@ import { TagStoreService } from '@services/tagStore.service';
 	styleUrls: ['./news-form.component.scss']
 })
 export default class NewsFormComponent implements OnInit {
+	@Input() tag: string;
 	userForm: FormGroup;
+	// private _router = inject(Router);
+	// private _activatedRoute = inject(ActivatedRoute);
 	private _formBuilder = inject(FormBuilder);
 	private _supabaseService = inject(SupabaseService);
 	private _location = inject(Location);
-	private _tagStoreService = inject(TagStoreService);
-	public tag = this._tagStoreService.getTag();
+	// private _tagStoreService = inject(TagStoreService);
+	// public path: string;
+	// public tag: string;
 
 	ngOnInit(): void {
+		// this.getTag();
+
 		this.userForm = this._formBuilder.group({
 			date: ['', Validators.required],
 			media: ['', Validators.required],
@@ -28,6 +36,19 @@ export default class NewsFormComponent implements OnInit {
 			url_new: ['', Validators.required]
 		});
 	}
+
+	// getTag() {
+	// 	const urlSegments = this._router.url.split('/');
+	// 	console.log('urlSegments:', urlSegments);
+
+	// 	// ¿Es ruta con parametro? Por ejemplo: path: 'licitaciones/:tag',
+	// 	if (urlSegments.length > 2) {
+	// 		this._activatedRoute.params.pipe(first()).subscribe(async ({ tag }) => {
+	// 			this.path = urlSegments[1];
+	// 			this.tag = tag;
+	// 		});
+	// 	}
+	// }
 
 	async guardar(): Promise<void> {
 		if (this.userForm?.valid) {
