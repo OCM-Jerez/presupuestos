685f5fb7cca651fc03a6bae4b135dc7a74030a89
mamjerez@gmail.com <mamjerez@gmail.com>
2024-03-23 13:53:43 +0100

    Refactor coms-form component styles and update input classes

--- src/app/addNewRecodsToSupabase/coms-form/coms-form.component.ts
+++ src/app/addNewRecodsToSupabase/coms-form/coms-form.component.ts
@@ -3,15 +3,13 @@ import { Component, Input, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { SupabaseService } from '@services/supabase.service';
-// import { TagStoreService } from '@services/tagStore.service';
 import { ModalService } from '@app/layouts/modal/modal.service';
 
 @Component({
 	selector: 'app-coms-form',
 	standalone: true,
 	imports: [FormsModule, ReactiveFormsModule],
-	templateUrl: './coms-form.component.html',
-	styleUrls: ['./coms-form.component.scss']
+	templateUrl: './coms-form.component.html'
 })
 export default class ComsFormComponent implements OnInit {
 	@Input() tag: string;
@@ -19,9 +17,7 @@ export default class ComsFormComponent implements OnInit {
 	private _formBuilder = inject(FormBuilder);
 	private _supabaseService = inject(SupabaseService);
 	private _location = inject(Location);
-	// private _tagStoreService = inject(TagStoreService);
 	private _modalService = inject(ModalService);
-	// public tag = this._tagStoreService.getTag();
 
 	ngOnInit(): void {
 		this.userForm = this._formBuilder.group({
